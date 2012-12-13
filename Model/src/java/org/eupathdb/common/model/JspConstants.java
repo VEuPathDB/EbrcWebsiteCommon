f100f94d7711f4c92ba35abd30fd395db895fd84
Cristina Aurrecoechea <aurreco@uga.edu>
2012-12-13 23:19:09 +0000

    OrthoMCL: publications that use Ortho
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@52204 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/java/org/eupathdb/common/model/JspConstants.java
+++ Model/src/java/org/eupathdb/common/model/JspConstants.java
@@ -3,6 +3,7 @@ package org.eupathdb.common.model;
 public class JspConstants {
 
   public String getPublicationUrl() { return PUBLICATION_URL; }
+		public String getOrthoGoogleUrl() { return ORTHO_GOOGLE_URL; }
   public String getYoutubeUrl() { return YOUTUBE_URL; }
 
   public static final String PUBLICATION_URL =
@@ -10,6 +11,12 @@ public class JspConstants {
       "as_oq=OrthoMCL+PlasmoDB+ToxoDB+CryptoDB+TrichDB+GiardiaDB+" +
       "TriTrypDB+AmoebaDB+MicrosporidiaDB+%22FungiDB%22+PiroplasmaDB+" +
       "ApiDB+EuPathDB&as_eq=encrypt+cryptography+hymenoptera&as_occt=any&" +
+      "as_sauthors=&as_publication=&as_ylo=&as_yhi=&as_sdt=1.&as_sdtp=on&" +
+      "as_sdtf=&as_sdts=39&btnG=Search+Scholar&hl=en";
+
+ public static final String ORTHO_GOOGLE_URL =
+      "http://scholar.google.com/scholar?as_q=&num=10&as_epq=&" +
+      "as_oq=OrthoMCL&as_eq=encrypt+cryptography+hymenoptera&as_occt=any&" +
       "as_sauthors=&as_publication=&as_ylo=&as_yhi=&as_sdt=1.&as_sdtp=on&" +
       "as_sdtf=&as_sdts=39&btnG=Search+Scholar&hl=en";
 
