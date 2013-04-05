0fa2c1cd98c1e305e6c11afa2b2355445615dd10
Dave Falke <dmfalke@gmail.com>
2013-04-05 16:58:30 +0000

    provide constants for WEBSITE_RELEASE_STAGE environment variable values. see https://mango.ctegd.uga.edu/apiwiki/index.php/WebsiteReleaseStages. refs #8580
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@54495 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/eupathdb/common/controller/WebsiteReleaseConstants.java
@@ -0,0 +1,21 @@
+package org.eupathdb.common.controller;
+
+public class WebsiteReleaseConstants {
+
+  public int getDevelopment() { return DEVELOPMENT; }
+  public int getIntegrate() { return INTEGRATE; }
+  public int getFeature() { return FEATURE; }
+  public int getAlpha() { return ALPHA; }
+  public int getQa() { return QA; }
+  public int getBeta() { return BETA; }
+  public int getWww() { return WWW; }
+
+  public static final int DEVELOPMENT = 10;
+  public static final int INTEGRATE = 20;
+  public static final int FEATURE = 30;
+  public static final int ALPHA = 40;
+  public static final int QA = 50;
+  public static final int BETA = 60;
+  public static final int WWW = 70;
+
+}
