5f14864331102dd3417a34270b5115cda12f05c4
Mark Heiges <mheiges@gmail.com>
2013-04-05 20:21:18 +0000

    more web site testing framework from ApiCommonWebsite to EuPathSiteCommon
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@54507 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Watar/src/java/org/apidb/apicommonwebsite/watar/SmokeTests.java
@@ -0,0 +1,111 @@
+package org.apidb.eupathsitecommon.watar;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.WebResponse;
+
+import java.net.URL;
+
+import org.apidb.eupathsitecommon.watar.Utilities;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.assertEquals;
+
+
+public class SmokeTests {
+
+    static final WebClient browser;
+    public String baseurl;
+    public String webappname;
+        
+    static {
+        browser = new WebClient();
+        browser.setThrowExceptionOnFailingStatusCode(false);
+        browser.setJavaScriptEnabled(false);
+    }
+
+    public SmokeTests() {
+        baseurl = System.getProperty("baseurl");
+        webappname = System.getProperty("webappname");
+    }
+
+    @Test(description="Assert HTTP header status is 200 OK for home page.",
+          groups = { "deployment" })
+    public void HomePage_HttpHeaderStatusIsOK() throws Exception {
+        String url = baseurl + "/" + webappname + "/";
+        assertHeaderStatusMessageIsOK(url);
+    }
+
+    @Test(description="Assert HTTP header status is 200 OK for WsfService url as test of Axis installation.",
+          groups = { "deployment", "webservice" })
+    public void WsfServicePage_HttpHeaderStatusIsOK() throws Exception {
+        String url = baseurl + "/" + webappname + Utilities.WSF_PATH;
+        assertHeaderStatusMessageIsOK(url);
+    }
+
+    @Test(description="Assert HTTP header status is 200 OK for News url as test of valid XML record.", 
+          groups = { "xmlrecord" })
+    public void News_HttpHeaderStatusIsOK() throws Exception {
+        String url = baseurl + "/" + webappname + Utilities.NEWS_PATH;
+        assertHeaderStatusMessageIsOK(url);
+    }
+
+    @Test(description="Assert HTTP header status is 200 OK for Methods url as test of valid XML record.", 
+          groups = { "xmlrecord" })
+    public void Methods_HttpHeaderStatusIsOK() throws Exception {
+        String url = baseurl + "/" + webappname + Utilities.METHODS_PATH;
+        assertHeaderStatusMessageIsOK(url);
+    }
+
+    @Test(description="Assert HTTP header status is 200 OK for GeneRecord page.",
+          groups = { "wdkrecord" },
+          dataProvider="defaultGeneId",
+          dependsOnMethods={"HomePage_HttpHeaderStatusIsOK"})
+    public void GeneRecordPage_HttpHeaderStatusIsOK(String geneId) throws Exception {
+        if (geneId == null) throw new Exception("unable to get gene id for testing");
+        String url = baseurl + "/" + webappname + Utilities.GENE_RECORD_PATH_TMPL + geneId;
+        assertHeaderStatusMessageIsOK(url);
+    }
+
+    /** 
+      * Returns the default value from the Gene ID Quick Search form on the front page.
+    **/
+    @DataProvider(name="defaultGeneId")
+    private Object[][] getDefaultGeneIdFromQuickSearchForm() throws Exception {
+        try {
+        String url = baseurl + "/";
+        WebRequest request = new WebRequest(new URL(url), HttpMethod.GET);
+        HtmlPage page = (HtmlPage) browser.getPage(request);
+        for (HtmlElement element : page.getElementsByTagName("input")) {
+            if (element.getAttribute("name").contains("single_gene_id"))
+                return new Object[][] {{ element.getAttribute("value") }};
+        }
+        return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+        
+    }
+
+    /** 
+      * Assert HEAD request returns 200 OK for the given url.
+    **/
+    private void assertHeaderStatusMessageIsOK(String url) throws Exception {
+
+        try {
+            WebRequest request = new WebRequest(new URL(url), HttpMethod.HEAD);
+            HtmlPage page = (HtmlPage) browser.getPage(request);
+            WebResponse response = page.getWebResponse();
+            assertEquals(response.getStatusMessage(), "OK",  "Wrong HTTP Status for " + url + ".");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+   
+    }
+}
