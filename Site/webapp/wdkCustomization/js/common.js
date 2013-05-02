7e3dce2a6747659b6f69506c3dbc344a4abeaae9
Dave Falke <dmfalke@gmail.com>
2013-05-02 17:05:50 +0000

    remove validation of missing fields that are actually optional. instead, make submit disabled until content has been filled in the message section.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@55445 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/common.js
+++ Site/webapp/wdkCustomization/js/common.js
@@ -21,6 +21,14 @@ wdk.util.namespace("eupath.setup", function(ns, $) {
       $(":file", fileRow).last().trigger("click");
     });
 
+
+    // enable/disable submit button based on content
+    var $submit = $form.find("[type='submit']");
+    $submit.prop("disabled", true);
+    $form.on("keyup", "[name='content']", function() {
+      $submit.prop("disabled", this.value.length === 0);
+    });
+
     // handle file changes
     $form.on("change", ":file", function(e) {
 
@@ -61,21 +69,21 @@ wdk.util.namespace("eupath.setup", function(ns, $) {
       $("body").on("submit", "#contact-us", function(e) {
         e.preventDefault();
         var form = this,
-            requiredFields = ['subject', 'reply', 'content'],
+            // requiredFields = ['subject', 'reply', 'content'],
             emailRegex = /^\S+@\S+$/,
             ccAddrs = this.addCc.value.split(/,\s*(?=\w)/);
 
         // validate required fields
-        var missingFields = _.filter(requiredFields, function(field) {
-          return !form[field].value;
-        });
+        // var missingFields = _.filter(requiredFields, function(field) {
+        //   return !form[field].value;
+        // });
 
-        if (missingFields.length) {
-          _.each(missingFields, function(field) {
-            $(form[field]).after("<span class='wdk-error'>This field is required</span>");
-          });
-          return;
-        }
+        // if (missingFields.length) {
+        //   _.each(missingFields, function(field) {
+        //     $(form[field]).after("<span class='wdk-error'>This field is required</span>");
+        //   });
+        //   return;
+        // }
 
         // validation of email addresses
         if (ccAddrs.length > 10) {
