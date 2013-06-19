a45f1c275e8654b0f37bbc5411efdd420c787a5c
Ryan Doherty <tech@conical.org>
2013-06-19 05:36:52 +0000

    #13621: commit everything needed to enable Java Melody and log4jdbc (both currently turned off)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@56473 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/eupathdb/common/controller/MelodyDisplayFilter.java
@@ -0,0 +1,68 @@
+package org.eupathdb.common.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MelodyDisplayFilter implements Filter {
+
+	// these values were borrowed from Java Melody
+	private static final String MONITORING_PATH_PARAM_NAME = "monitoring-path";
+	private static final String DEFAULT_MONITORING_PATH = "/monitoring";
+	
+	private String _gusHome;
+	private String _monitoringPath;
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+
+		_monitoringPath = (config.getInitParameter(MONITORING_PATH_PARAM_NAME) == null ?
+				DEFAULT_MONITORING_PATH : config.getInitParameter(MONITORING_PATH_PARAM_NAME));
+		
+		_gusHome = config.getServletContext().getRealPath(
+				config.getServletContext().getInitParameter("GUS_HOME"));
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (isHttpRequest(request, response) &&  // if not HTTP request, then Melody will ignore
+		    isMelodyDisplayUrl(request) &&       // if not monitoring URL, then let through
+		    !isMelodyDisplayEnabled())           // if display enabled, then let through
+		{
+	      // need to deny access to this URL so Melody does not display interface
+	      HttpServletResponse httpResponse = (HttpServletResponse)response;
+		  httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Page Unavailable.");
+		}
+		// all other requests are passed through
+		chain.doFilter(request, response);
+		return;
+	}
+
+	private boolean isHttpRequest(ServletRequest request, ServletResponse response) {
+		return (request instanceof HttpServletRequest) && (response instanceof HttpServletResponse);
+	}
+
+	private boolean isMelodyDisplayEnabled() {
+		return new File(_gusHome + "/config/MELODY_ENABLED").exists();
+	}
+
+	private boolean isMelodyDisplayUrl(ServletRequest request) {
+		// assume request cast has already been checked
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		return httpRequest.getRequestURI().equals(httpRequest.getContextPath() + _monitoringPath);
+	}
+
+	@Override
+	public void destroy() {
+		// do nothing
+	}
+}
---
+++ Model/src/java/org/eupathdb/common/db/EuPathDbDriverInitializer.java
@@ -0,0 +1,36 @@
+package org.eupathdb.common.db;
+
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.gusdb.wdk.model.dbms.DbDriverInitializer;
+
+public class EuPathDbDriverInitializer implements DbDriverInitializer {
+
+	public static final Logger LOG = Logger.getLogger(EuPathDbDriverInitializer.class);
+	
+	private static final String MELODY_DRIVER = "net.bull.javamelody.JdbcDriver";
+	
+	private static final String LOG4JDBC_DRIVER = "net.sf.log4jdbc.DriverSpy";
+	private static final String LOG4JDBC_URL_PREFIX = "jdbc:log4";
+	
+	@Override
+	public String initializeDriver(String driverClassName, String connectionUrl, Properties props) throws ClassNotFoundException {
+		
+		// NOTE!!  The order of the registration of these drivers is important.  Log4Jdbc first, then Melody
+		LOG.debug("Initializing Log4Jdbc proxy driver.");
+		Class.forName(LOG4JDBC_DRIVER);
+		
+		LOG.debug("Initializing Java Melody JDBC driver.");
+		Class.forName(MELODY_DRIVER);
+		
+		LOG.debug("Specifying native JDBC driver to Melody (" + driverClassName + ").");
+		props.put("driver", driverClassName);
+		
+		LOG.debug("Prepending Log4Jdbc URL prefix onto connection URL.");
+		connectionUrl = LOG4JDBC_URL_PREFIX + connectionUrl;
+		
+		return connectionUrl;
+	}
+
+}
