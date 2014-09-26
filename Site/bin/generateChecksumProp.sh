fc41c2e6d20354a8ad608654d7ed83f0df95a568
Dave Falke <dmfalke@gmail.com>
2014-09-26 18:05:09 +0000

    wrap logic for checksum props in shell script
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@64761 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/bin/generateChecksumProp.sh
+++ Site/bin/generateChecksumProp.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-PROP_FILE=$WEBAPP/WEB-INF/assets-checksums.properties
-
 cd $WEBAPP;
 
 ASSETS=`find . -name *.js -o -name *.css -o -name *.jpg -o -name *.jpeg -o -name *.png -o -name *.gif`
 
 # generate checksums, strip leading dir chars, swap columns and join with "=", then write to props file
-md5sum $ASSETS | sed 's:\./::' | awk '{OFS = "="}{print $2, $1}' > $PROP_FILE
+md5sum $ASSETS | sed 's:\./::' | awk '{OFS = "="}{print $2, $1}'
