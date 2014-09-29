b0592a64c28da7f555ff81a6e84ed3a18e871592
Dave Falke <dmfalke@gmail.com>
2014-09-29 16:18:26 +0000

    remove bin directory
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@64772 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/bin/generateChecksumProp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd $WEBAPP;
-
-ASSETS=`find . -name *.js -o -name *.css -o -name *.jpg -o -name *.jpeg -o -name *.png -o -name *.gif`
-
-# generate checksums, strip leading dir chars, swap columns and join with "=", then write to props file
-md5sum $ASSETS | sed 's:\./::' | awk '{OFS = "="}{print $2, $1}'
