7456f1975fce0cf85ddde6f820fc1d4132bd9e1e
Ryan Doherty <tech@conical.org>
2017-01-13 20:50:22 +0000

    Initial script for setting up a migration environment
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@79225 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/data/migration/create_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p gus_home/config project_home yaml control
+export GUS_HOME=$(pwd)/gus_home
+export PROJECT_HOME=$(pwd)/project_home
+cd $PROJECT_HOME
+svn co https://www.cbil.upenn.edu/svn/gus/install/trunk install
+svn co https://www.cbil.upenn.edu/svn/gus/FgpUtil/trunk FgpUtil
+cd ..
+source $PROJECT_HOME/install/bin/gusEnv.bash
+bld FgpUtil
+cd $PROJECT_HOME
+rm -rf FgpUtil install
+fgpCheckout allsite trunk
+cd ..
+bld EuPathPresenters
+bld OrthoMCLWebsite
+cp $PROJECT_HOME/EuPathSiteCommon/Model/lib/yaml/metaConfig.yaml.sample yaml/PROJECTMetaConfig.yaml
