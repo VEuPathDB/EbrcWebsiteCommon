038b84ea1907dd6740240ecfff1a29447a190921
Dave Falke <dmfalke@gmail.com>
2017-01-19 21:48:11 +0000

    add links and relax gene id condition #24771
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@79317 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
+++ Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
@@ -100,12 +100,14 @@ const siteAnnouncements = [
   },
 
   // 3'UTR sequences for Tb927 genes
-  ({ projectId, location }) => projectId === 'TriTrypDB' && location.pathname.includes('/record/gene/Tb927.') && (
+  ({ projectId, location }) => projectId === 'TriTrypDB' && location.pathname.includes('/record/gene/Tb') && (
     <div key="tb927-3'utr">
       We discovered a mistake in the representation of 3'UTR sequences for
-      Tb927 genes and are working to correct the problem. Please refer to
-      TriTrypDB Release 28 when viewing or downloading Tb927 3'UTR sequences. A
-      Tb927 gff file without UTRs is available at Sanger's ftp site.
+      Tb927 genes and are working to correct the problem. Please refer to <a
+        href="http://r28.tritrypdb.org/tritrypdb.b28/">TriTrypDB Release 28
+        </a> when viewing or downloading Tb927 3'UTR sequences. A <a
+        href="ftp://ftp.sanger.ac.uk/pub/project/pathogens/gff3/CURRENT/">
+        Tb927 gff file without UTRs</a> is available at Sanger's ftp site.
     </div>
   )
 
