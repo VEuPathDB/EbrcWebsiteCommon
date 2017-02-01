c779fbb7a9e84c938ebeb6419cdd2fbc1695ea01
Ryan Doherty <tech@conical.org>
2017-02-01 03:15:21 +0000

    Add param to framework script to use branched code and modify instructions to reflect
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/trunk@79554 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/data/migration/build_framework.sh
+++ Model/data/migration/build_framework.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+if [ "$#" != "1" ]; then
+  echo 'USAGE: build_framework.sh ["trunk"|<svn_branch>]'
+  exit 1
+fi
+
+branch=$1
+branchPath=$branch
+if [ "$branch" != "trunk" ]; then
+  branchPath=branches/$branch
+fi
+
 mkdir -p gus_home/config project_home yaml control
 export GUS_HOME=$(pwd)/gus_home
 export PROJECT_HOME=$(pwd)/project_home
 echo "perl=/usr/bin/perl" > $GUS_HOME/config/gus.config
 cd $PROJECT_HOME
-svn co https://www.cbil.upenn.edu/svn/gus/install/trunk install
-svn co https://www.cbil.upenn.edu/svn/gus/FgpUtil/trunk FgpUtil
+svn co https://www.cbil.upenn.edu/svn/gus/install/$branchPath install
+svn co https://www.cbil.upenn.edu/svn/gus/FgpUtil/$branchPath FgpUtil
 cd ..
 source $PROJECT_HOME/install/bin/gusEnv.bash
 bld FgpUtil
 cd $PROJECT_HOME
 rm -rf FgpUtil install
-fgpCheckout allsite trunk
+fgpCheckout allsite $branch
 cd ..
 bld EuPathPresenters
 bld OrthoMCLWebsite
