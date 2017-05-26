73bc30f3cf715c6e66b23f660a74395869e4dc1a
Dave Falke <dmfalke@gmail.com>
2017-05-26 20:38:02 +0000

    Move all state to QuestionWizardController and add appropriate filter param service calls.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@81970 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/QuestionWizard.jsx
+++ Site/webapp/wdkCustomization/js/client/components/QuestionWizard.jsx
@@ -1,62 +1,53 @@
 import React from 'react';
-import { memoize } from 'lodash';
 import FilterParamNew from './FilterParamNew';
 
-export default class QuestionWizard extends React.Component {
-
-  static createEmptyState() {
-    return {
-      activeGroup: undefined
-    };
-  }
-
-  constructor(props) {
-    super(props);
-    this.setActiveGroup = this.setActiveGroup.bind(this);
-  }
-
-  setActiveGroup(activeGroup) {
-    this._update({ activeGroup });
-  }
-
-  _update(nextState) {
-    this.props.onChange(Object.assign({}, this.props.wizardState, nextState));
-  }
-
-  render() {
-    const { question, wizardState } = this.props;
-    const { activeGroup } = wizardState;
-    return (
-      <div>
-        <h1>{question.displayName}</h1>
-        <p>{question.summary}</p>
-        <GroupList groups={question.groups} onGroupSelect={this.setActiveGroup}/>
-        <div>
-          {activeGroup == null ? (
-            <div>Select a group above to get started</div>
-          ) : (
-            <div>
-              <p>{activeGroup.description}</p>
-              <ul>
-                {activeGroup.parameters.map(renderParam(question))}
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
-    )
-  }
-
-}
-
-const renderParam = question => paramName => {
-  const param = question.parameters.find(p => p.name === paramName);
-  const { Component, handler } = findParamStuff(param);
+export default function QuestionWizard(props) {
+  const {
+    question,
+    paramValues,
+    paramUIState,
+    recordClass,
+    activeGroup,
+    onActiveGroupChange,
+    onActiveOntologyTermChange,
+    onParamValueChange
+  } = props;
   return (
-    <li key={paramName}>
-      <Component param={param} handler={handler}/>
-    </li>
-  );
+    <div>
+      <h1>{recordClass.displayName} by {question.displayName}</h1>
+      <p>{question.summary}</p>
+      <GroupList
+        groups={question.groups}
+        onGroupSelect={onActiveGroupChange}
+      />
+      <div>
+        {activeGroup == null ? (
+          <div>Select a group above to get started</div>
+        ) : (
+          <div>
+            <p>{activeGroup.description}</p>
+            <ul>
+              {activeGroup.parameters.map(paramName => {
+                const param = question.parameters.find(p => p.name === paramName);
+                const ParamComponent = findParamComponent(param);
+                return (
+                  <li key={paramName}>
+                    <ParamComponent
+                      param={param}
+                      value={paramValues[param.name]}
+                      uiState={paramUIState[param.name]}
+                      onActiveOntologyTermChange={onActiveOntologyTermChange}
+                      onParamValueChange={onParamValueChange}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 function GroupList(props) {
@@ -81,45 +72,9 @@ function Param(props) {
   )
 }
 
-function findParamStuff(param) {
+function findParamComponent(param) {
   if (param.type === 'FilterParamNew') {
-    return {
-      Component: FilterParamNew,
-      handler: FilterParamNewHandler
-    };
+    return FilterParamNew;
   }
-  return {
-    Component: Param,
-    handler: {}
-  };
-}
-
-const FilterParamNewHandler = (questionName, paramName) => ({
-
-  getActiveFieldSummary: memoize((activeField) => {
-    return fetch(`/a/service/question/${questionName}/${paramName}/ontologyTermSummary`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      body: JSON.stringify({
-        ontologyId: activeField.ontologyId,
-        filters: [],
-        contextParamValues: {}
-      })
-    })
-      .then(response => response.json())
-      .then(formatSummary);
-  })
-
-})
-
-function formatSummary(summary) {
-  return Object.keys(summary).map(value => ({
-    value,
-    count: summary[value].unfiltered,
-    filteredCount: summary[value].filtered
-  }));
+  return Param;
 }
--- Site/webapp/wdkCustomization/js/client/controllers/QuestionWizardController.jsx
+++ Site/webapp/wdkCustomization/js/client/controllers/QuestionWizardController.jsx
@@ -5,22 +5,190 @@ export default class QuestionWizardController extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleWizardChange = wizardState => this.setState({ wizardState });
-    this.state = {
-      wizardState: QuestionWizard.createEmptyState()
-    };
+    this.state = { };
+    this.onActiveGroupChange = this.onActiveGroupChange.bind(this);
+    this.onActiveOntologyTermChange = this.onActiveOntologyTermChange.bind(this);
+    this.onParamValueChange = this.onParamValueChange.bind(this);
   }
 
+  loadQuestion(props) {
+    const { questionName, wdkService } = props;
+    const question$ = wdkService.sendRequest({
+      method: 'GET',
+      path: '/question/' + questionName,
+      params: { expandParams: true }
+    });
+
+    const recordClass$ = question$.then(question => {
+      return wdkService.findRecordClass(rc => rc.name === question.recordClassName);
+    });
+
+    Promise.all([ question$, recordClass$ ])
+    .then(
+      ([ question, recordClass ]) => {
+        document.title = `Search for ${recordClass.displayName} by ${question.displayName}`;
+
+        const paramValues = question.parameters.reduce(function(paramValues, param) {
+          return Object.assign(paramValues, {
+            [param.name]: param.defaultValue
+          });
+        }, {});
+
+        const paramUIState = question.parameters.reduce(function(uiState, param) {
+          switch(param.type) {
+            case 'FilterParamNew':
+              return Object.assign(uiState, {
+                [param.name]: {
+                  activeOntologyTerm: undefined,
+                  ontologyTermSummaries: {}
+                }
+              });
+
+            default:
+              return Object.assign(uiState, {
+                [param.name]: {}
+              });
+          }
+        }, {});
+
+        this.setState({
+          question,
+          paramValues,
+          paramUIState,
+          recordClass,
+          activeGroup: undefined
+        });
+
+        question.parameters.forEach(param => {
+          if (param.type === 'FilterParamNew') {
+            this.props.wdkService.getFilterParamSummaryCounts(
+              question.urlSegment,
+              param.name,
+              JSON.parse(paramValues[param.name]).filters || [],
+              paramValues
+            ).then(counts => {
+              this._updateParamUIState(param.name, { counts });
+            })
+          }
+        });
+
+      },
+      error => {
+        this.setState({ error });
+      }
+    );
+  }
+
+  onActiveGroupChange(activeGroup) {
+    this.setState({ activeGroup });
+  }
+
+  onActiveOntologyTermChange(param, filters, ontologyTerm) {
+    this._updateParamUIState(param.name, {
+      activeOntologyTerm: ontologyTerm
+    });
+    this.props.wdkService.getOntologyTermSummary(
+      this.state.question.urlSegment,
+      param.name,
+      filters.filter(filter => filter.field !== ontologyTerm),
+      ontologyTerm,
+      this.state.paramValues
+    ).then(ontologyTermSummary => {
+      const { ontologyTermSummaries } = this.state.paramUIState[param.name];
+      this._updateParamUIState(param.name, {
+        ontologyTermSummaries: Object.assign({}, ontologyTermSummaries, {
+          [ontologyTerm]: formatSummary(ontologyTermSummary)
+        })
+      });
+    });
+  }
+
+  onParamValueChange(param, paramValue) {
+    this.setState({
+      paramValues: Object.assign({}, this.state.paramValues, {
+        [param.name]: paramValue
+      })
+    });
+
+    if (param.type === 'FilterParamNew') {
+
+      const { filters = [] } = JSON.parse(paramValue);
+      const { activeOntologyTerm } = this.state.paramUIState[param.name];
+
+      this.props.wdkService.getFilterParamSummaryCounts(
+        this.state.question.urlSegment,
+        param.name,
+        filters,
+        this.state.paramValues
+      ).then(counts => {
+        this._updateParamUIState(param.name, { counts });
+      });
+
+      this.props.wdkService.getOntologyTermSummary(
+        this.state.question.urlSegment,
+        param.name,
+        filters.filter(filter => filter.field !== activeOntologyTerm),
+        activeOntologyTerm,
+        this.state.paramValues
+      ).then(ontologyTermSummary => {
+        const { ontologyTermSummaries } = this.state.paramUIState[param.name];
+        this._updateParamUIState(param.name, {
+          ontologyTermSummaries: Object.assign({}, ontologyTermSummaries, {
+            [activeOntologyTerm]: formatSummary(ontologyTermSummary)
+          })
+        });
+      });
+
+    }
+    // this.props.wdkService.getQuestionParamValues(
+    //   this.state.question.urlSegment,
+    //   paramName,
+    //   paramValue,
+    //   this.state.paramValues
+    // ).then(question => {
+    // TODO I think we need to update both paramValues and paramUIState with `question.parameters` - dmf
+    // })
+  }
+
+  _updateParamUIState(paramName, newState) {
+    this.setState({
+      paramUIState: Object.assign({}, this.state.paramUIState, {
+        [paramName]: Object.assign({}, this.state.paramUIState[paramName], newState)
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.loadQuestion(this.props);
+  }
+
+  componentDidWillReceiveProps(nextProps) {
+    this.loadQuestion(nextProps);
+  }
 
   render() {
+    if (this.state.question == null) return null;
+
     return (
       <QuestionWizard 
-        question={this.props.question}
-        paramHandlers={this.paramHandlers}
-        wizardState={this.state.wizardState}
-        onChange={this.handleWizardChange}
+        question={this.state.question}
+        recordClass={this.state.recordClass}
+        activeGroup={this.state.activeGroup}
+        paramValues={this.state.paramValues}
+        paramUIState={this.state.paramUIState}
+        onActiveGroupChange={this.onActiveGroupChange}
+        onActiveOntologyTermChange={this.onActiveOntologyTermChange}
+        onParamValueChange={this.onParamValueChange}
       />
     )
   }
 
+}
+
+function formatSummary(summary) {
+  return Object.keys(summary).map(value => ({
+    value,
+    count: summary[value].unfiltered,
+    filteredCount: summary[value].filtered
+  }));
 }
