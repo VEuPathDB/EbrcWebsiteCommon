fc082c614e8feab6cc406238fc10ff2ff6ce7b80
Dave Falke <dmfalke@gmail.com>
2017-07-17 16:43:49 +0000

    Move common wrappers from apicommon to ebrc, and improve some comments and code.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@83262 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/component-wrappers/index.js
+++ Site/webapp/wdkCustomization/js/client/component-wrappers/index.js
@@ -1,4 +1,5 @@
-export * from './Header';
 export * from './DownloadForm';
 export * from './Footer';
+export * from './Header';
 export * from './RecordHeading';
+export * from './RecordMainSection';
