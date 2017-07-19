f6edfb5edfa8a451d8a8af8080b232c18f73b5dc
Ryan Doherty <tech@conical.org>
2017-07-19 19:25:54 +0000

    Simplify code for Java 8
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@83306 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/controller/EuPathSiteSetup.java
+++ Model/src/main/java/org/eupathdb/common/controller/EuPathSiteSetup.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 import org.eupathdb.common.errors.ErrorHandler;
 import org.eupathdb.common.errors.ErrorHandlerHelpers;
 import org.eupathdb.common.errors.ErrorHandlerHelpers.ErrorCategory;
-import org.gusdb.fgputil.events.Event;
 import org.gusdb.fgputil.events.EventListener;
 import org.gusdb.fgputil.events.Events;
 import org.gusdb.wdk.events.ErrorEvent;
@@ -34,15 +33,13 @@ public class EuPathSiteSetup {
   private static void addErrorListener() {
     try {
       // only load filters once from disk
-      final Properties filters = ErrorHandlerHelpers.loadErrorFilters();
-      final List<ErrorCategory> categories = ErrorHandlerHelpers.loadErrorCategories();
-      Events.subscribe(new EventListener() {
-        @Override
-        public void eventTriggered(Event event) throws Exception {
-          ErrorEvent error = (ErrorEvent)event;
-          new ErrorHandler(filters, categories).handleError(error.getErrorBundle(), error.getErrorContext());
-        }
-      }, ErrorEvent.class);
+      Properties filters = ErrorHandlerHelpers.loadErrorFilters();
+      List<ErrorCategory> categories = ErrorHandlerHelpers.loadErrorCategories();
+      EventListener listener = event -> {
+        ErrorEvent error = (ErrorEvent)event;
+        new ErrorHandler(filters, categories).handleError(error.getErrorBundle(), error.getErrorContext());
+      };
+      Events.subscribe(listener, ErrorEvent.class);
     }
     catch (Exception e) {
       String message = "Could not load error email filters or categories.";
