242b498522a1e205dbdd2808c9badcace95b862f
Austin Billings <austin@austinbillings.com>
2018-01-12 00:56:23 +0000

    Remove references to <ModalBoundary>, add style for new BodyLayer system
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@86312 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/controllers/HeaderController.jsx
+++ Site/webapp/wdkCustomization/js/client/controllers/HeaderController.jsx
@@ -1,9 +1,8 @@
 import { WdkViewController } from 'wdk-client/Controllers';
 import { Header } from 'wdk-client/Components';
-import { ModalBoundary } from 'mesa';
 
 export default class HeaderController extends WdkViewController {
   renderView() {
-    return <ModalBoundary style={{ zIndex: 1000 }}><Header/></ModalBoundary>
+    return <Header/>;
   }
 }
