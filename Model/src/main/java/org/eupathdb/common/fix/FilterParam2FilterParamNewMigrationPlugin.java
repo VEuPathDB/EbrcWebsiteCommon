401f022bd050135639c50f8caa0cceccaf6cede4
Ryan Doherty <tech@conical.org>
2018-01-25 15:48:32 +0000

    Initial framework for migration script to convert FilterParam values to FilterParmaNew values
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@86648 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/eupathdb/common/fix/FilterParam2FilterParamNewMigrationPlugin.java
@@ -0,0 +1,96 @@
+package org.eupathdb.common.fix;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.gusdb.fgputil.ListBuilder;
+import org.gusdb.fgputil.json.JsonUtil;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.fix.table.TableRowInterfaces.RowResult;
+import org.gusdb.wdk.model.fix.table.TableRowInterfaces.TableRowUpdaterPlugin;
+import org.gusdb.wdk.model.fix.table.TableRowUpdater;
+import org.gusdb.wdk.model.fix.table.steps.StepData;
+import org.gusdb.wdk.model.fix.table.steps.StepDataFactory;
+import org.gusdb.wdk.model.fix.table.steps.StepDataWriter;
+import org.gusdb.wdk.model.query.param.FilterParam;
+import org.gusdb.wdk.model.query.param.Param;
+import org.gusdb.wdk.model.user.Step;
+import org.json.JSONObject;
+
+public class FilterParam2FilterParamNewMigrationPlugin implements TableRowUpdaterPlugin<StepData> {
+
+  private static final Logger LOG = Logger.getLogger(FilterParam2FilterParamNewMigrationPlugin.class);
+
+  private static enum ResultType {
+    BAD_QUESTION,
+    NO_CHANGE,
+    VALUE_CHANGED,
+    ERROR
+  }
+
+  private WdkModel _wdkModel;
+  private int[] _counts = { 0, 0, 0, 0 };
+
+  @Override
+  public void configure(WdkModel wdkModel, List<String> additionalArgs) throws Exception {
+    _wdkModel = wdkModel;
+  }
+
+  @Override
+  public TableRowUpdater<StepData> getTableRowUpdater(WdkModel wdkModel) {
+    return new TableRowUpdater<StepData>(new StepDataFactory(false),
+        ListBuilder.asList(new StepDataWriter()), this, wdkModel);
+  }
+
+  @Override
+  public RowResult<StepData> processRecord(StepData nextRow) throws Exception {
+    try {
+      RowResult<StepData> result = new RowResult<>(nextRow);
+      Map<String, Param> questionParams;
+      try {
+        questionParams = _wdkModel.getQuestion(nextRow.getQuestionName()).getParamMap();
+      }
+      catch(WdkModelException e) {
+        // question name is invalid; skip this row without error
+        _counts[ResultType.BAD_QUESTION.ordinal()]++;
+        return result;
+      }
+      JSONObject json = nextRow.getParamFilters();
+      JSONObject params = json.getJSONObject(Step.KEY_PARAMS);
+      boolean valueChanged = false;
+      for (String paramName : JsonUtil.getKeys(params)) {
+        if (questionParams.get(paramName) instanceof FilterParam) {
+          // need to convert to FilterParamNew
+          JSONObject oldValue = new JSONObject(params.getString(paramName));
+          JSONObject newValue = convertParamValue(oldValue);
+          params.put(paramName, newValue.toString());
+          valueChanged = true;
+        }
+        _counts[(valueChanged ? ResultType.VALUE_CHANGED : ResultType.NO_CHANGE).ordinal()]++;
+      }
+      return result.setShouldWrite(valueChanged);
+    }
+    catch (Exception e) {
+      _counts[ResultType.ERROR.ordinal()]++;
+      throw e;
+    }
+  }
+
+  private JSONObject convertParamValue(JSONObject oldValue) {
+
+    // TODO convert FilterParam(Old) value to FilterParamNew value
+
+    return oldValue;
+  }
+
+  @Override
+  public void dumpStatistics() {
+    LOG.info("Plugin Statistics:");
+    for (ResultType type : ResultType.values()) {
+      LOG.info("  " + type.toString() + ": " + _counts[type.ordinal()]);
+    }
+  }
+
+}
