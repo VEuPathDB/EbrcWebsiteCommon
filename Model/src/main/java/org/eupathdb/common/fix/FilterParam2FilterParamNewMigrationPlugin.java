a07d3e5bdc9929e948013f8a97f042f43638882c
Ryan Doherty <tech@conical.org>
2018-01-29 18:38:52 +0000

    improvement based on better understanding of the migration
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@86740 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/fix/FilterParam2FilterParamNewMigrationPlugin.java
+++ Model/src/main/java/org/eupathdb/common/fix/FilterParam2FilterParamNewMigrationPlugin.java
@@ -14,7 +14,7 @@ import org.gusdb.wdk.model.fix.table.TableRowUpdater;
 import org.gusdb.wdk.model.fix.table.steps.StepData;
 import org.gusdb.wdk.model.fix.table.steps.StepDataFactory;
 import org.gusdb.wdk.model.fix.table.steps.StepDataWriter;
-import org.gusdb.wdk.model.query.param.FilterParam;
+import org.gusdb.wdk.model.query.param.FilterParamNew;
 import org.gusdb.wdk.model.query.param.Param;
 import org.gusdb.wdk.model.user.Step;
 import org.json.JSONObject;
@@ -61,12 +61,14 @@ public class FilterParam2FilterParamNewMigrationPlugin implements TableRowUpdate
       JSONObject params = json.getJSONObject(Step.KEY_PARAMS);
       boolean valueChanged = false;
       for (String paramName : JsonUtil.getKeys(params)) {
-        if (questionParams.get(paramName) instanceof FilterParam) {
+        if (questionParams.get(paramName) instanceof FilterParamNew) {
           // need to convert to FilterParamNew
           JSONObject oldValue = new JSONObject(params.getString(paramName));
           JSONObject newValue = convertParamValue(oldValue);
-          params.put(paramName, newValue.toString());
-          valueChanged = true;
+          if (newValue != null) {
+            params.put(paramName, newValue.toString());
+            valueChanged = true;
+          }
         }
       }
       _counts[(valueChanged ? ResultType.VALUE_CHANGED : ResultType.NO_CHANGE).ordinal()]++;
@@ -78,6 +80,14 @@ public class FilterParam2FilterParamNewMigrationPlugin implements TableRowUpdate
     }
   }
 
+  /**
+   * Takes a look at JSON value of a FilterParamNew.  Format MAY be that of an old FilterParam.  Detects this
+   * and if it is in the old format, creates a new JSONObject representing the new format and returns it.  If
+   * oldValue is already in the new format, returns null.
+   * 
+   * @param oldValue JSON value of a FilterParamNew
+   * @return new JSON value, or null if already in FilterParamNew format
+   */
   private JSONObject convertParamValue(JSONObject oldValue) {
 
     // TODO convert FilterParam(Old) value to FilterParamNew value
