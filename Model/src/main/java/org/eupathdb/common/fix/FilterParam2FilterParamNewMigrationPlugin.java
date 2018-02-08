4b55405b8ebf41e4775009311d03ad5e492f8862
Dave Falke <dmfalke@gmail.com>
2018-02-08 20:06:28 +0000

    initial filterParamNew migration plugin conversion implementation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@87121 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/fix/FilterParam2FilterParamNewMigrationPlugin.java
+++ Model/src/main/java/org/eupathdb/common/fix/FilterParam2FilterParamNewMigrationPlugin.java
@@ -1,10 +1,12 @@
 package org.eupathdb.common.fix;
 
+import java.text.DateFormat;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.gusdb.fgputil.ListBuilder;
+import org.gusdb.fgputil.json.JsonType;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
@@ -17,6 +19,8 @@ import org.gusdb.wdk.model.fix.table.steps.StepDataWriter;
 import org.gusdb.wdk.model.query.param.FilterParamNew;
 import org.gusdb.wdk.model.query.param.Param;
 import org.gusdb.wdk.model.user.Step;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class FilterParam2FilterParamNewMigrationPlugin implements TableRowUpdaterPlugin<StepData> {
@@ -88,15 +92,151 @@ public class FilterParam2FilterParamNewMigrationPlugin implements TableRowUpdate
    * Takes a look at JSON value of a FilterParamNew.  Format MAY be that of an old FilterParam.  Detects this
    * and if it is in the old format, creates a new JSONObject representing the new format and returns it.  If
    * oldValue is already in the new format, returns null.
-   * 
+   *
+   *
+   * The format for FilterParamNew stable value is as follows (in TypeScript):
+   *
+   *    interface MemberValue<T> extends Array<T> {}
+   *
+   *    interface RangeValue<T> {
+   *      min: T | null;
+   *      max: T | null;
+   *    }
+   *
+   *    interface FilterBase {
+   *      field: string;
+   *      type: string;
+   *      isRange: boolean;
+   *      includeUnknown: boolean;
+   *      value: any;
+   *    }
+   *
+   *    interface StringFilter extends FilterBase {
+   *      type: 'string';
+   *      value: MemberValue<string>;
+   *    }
+   *
+   *    interface DateFilter extends FilterBase {
+   *      type: 'date';
+   *      value: MemberValue<string> | RangeValue<string>;
+   *    }
+   *
+   *    interface NumberFilter extends FilterBase {
+   *      type: 'number';
+   *      value: MemberValue<number> | RangeValue<number>;
+   *    }
+   *
+   *    type Filter = StringFilter | DateFilter | NumberFilter;
+   *
+   *    interface FilterParamStableValue {
+   *      filters: Filter[]
+   *    }
+   *
+   *
+   * The primary differences between the old FilterParam stable value and the new FilterParam
+   * stable value are:
+   *   - old doesn't have `isRange` or `includeUnknown`
+   *   - old does have some extra properties that are not needed: `values`, `ignored`, `selection`
+   *
    * @param oldValue JSON value of a FilterParamNew
    * @return new JSON value, or null if already in FilterParamNew format
    */
-  private JSONObject convertParamValue(JSONObject oldValue) {
-
+  private JSONObject convertParamValue(JSONObject oldValue) throws Exception {
     // TODO convert FilterParam(Old) value to FilterParamNew value
 
-    return oldValue;
+    // If `values` property is missing, assume it is FilterParamNew stable value.
+    if (!oldValue.has("values")) {
+      System.out.println("Skipping param since value appears new.");
+      return oldValue;
+    }
+
+    if (!oldValue.has("filters")) oldValue.put("filters", new JSONArray());
+
+    JSONArray filtersJson = oldValue.getJSONArray("filters");
+
+    JSONArray newFiltersJson = new JSONArray();
+
+    for (int i = 0; i < filtersJson.length(); i++ ) {
+      JSONObject filterJson = filtersJson.getJSONObject(i);
+      String field = filterJson.getString("field");
+
+      // Need to infer `type` from `value`
+      String type;
+      Object value;
+      Boolean isRange;
+      Boolean includeUnknown = false;
+
+      try {
+        JSONArray memberValueJson = filterJson.getJSONArray("value");
+        isRange = false;
+        for (int j = 0; j < memberValueJson.length(); j++) {
+          if (memberValueJson.isNull(j) || memberValueJson.get(j).equals("unknown")) {
+            includeUnknown = true;
+            memberValueJson.remove(j);
+            j--;
+          }
+        }
+        if (memberValueJson.length() == 0) {
+          type = "string";
+        }
+        else {
+          JsonType jsonType = new JsonType(memberValueJson.get(0));
+          switch (jsonType.getType()) {
+            case STRING:
+              type = "string";
+              break;
+
+            case NUMBER:
+              type = "number";
+              break;
+
+            default:
+              throw new Exception("Unknown value type: " + jsonType.getType() + "\n" + memberValueJson.toString(4));
+          }
+        }
+        value = memberValueJson;
+      }
+      catch (JSONException memberEx) {
+       try {
+         JSONObject rangeValueJSON = filterJson.getJSONObject("value");
+         isRange = true;
+         JsonType minType = new JsonType(rangeValueJSON.has("min") ? rangeValueJSON.get("min") : JSONObject.NULL);
+         JsonType maxType = new JsonType(rangeValueJSON.has("max") ? rangeValueJSON.get("max") : JSONObject.NULL);
+         JsonType sampleType = minType.isNull() ? maxType : minType;
+
+         switch(sampleType.getType()) {
+           case STRING:
+             if (!minType.isNull()) rangeValueJSON.put("min", Double.parseDouble(rangeValueJSON.getString("min")));
+             if (!maxType.isNull()) rangeValueJSON.put("max", Double.parseDouble(rangeValueJSON.getString("max")));
+             type = "number";
+             break;
+
+           case NUMBER:
+           case NULL:
+             type = "number";
+             break;
+
+           default:
+             throw new Exception("Unknown range type: " + sampleType.getType());
+         }
+         value = rangeValueJSON;
+       }
+       catch(JSONException rangeEx) {
+         throw new Exception("Unexpected `value` property in filter JSON:\n" + filterJson.toString(4));
+       }
+      }
+
+      newFiltersJson.put(i, new JSONObject()
+          .put("field", field)
+          .put("type", type)
+          .put("isRange", isRange)
+          .put("includeUnknown", includeUnknown)
+          .put("value", value));
+    }
+    JSONObject newValue = new JSONObject()
+        .put("filters", newFiltersJson);
+    System.out.println("Updated stable value:\n" + newValue.toString(4));
+    return newValue;
   }
 
   @Override
