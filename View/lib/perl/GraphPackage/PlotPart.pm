ee0986d28e5c25a031fd143b2199d03184de11e8
Danielle Callan <dcallan@upenn.edu>
2018-02-23 16:13:10 +0000

    fix size of text in pathway graphs without data
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/branches/api-build-36@87421 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/GraphPackage/PlotPart.pm
+++ View/lib/perl/GraphPackage/PlotPart.pm
@@ -250,17 +250,23 @@ sub blankGGPlotPart {
 my ($self)= @_;
 my $plotTitle = $self->getPlotTitle();
 
-my $text = "None";
+my $text = "\"None\"";
+my $textSize = 10;
 
-if($self->isCompact()) {
-  $text = $self->getId();
+my $idType = $self->getIdType();
+
+if(($idType) && lc($idType) eq 'ec') {
+  if($self->isCompact()) {
+    $text = "\"" . $self->getId() . "\"";
+    $textSize = 4;
+  }
 }
 
 return <<DummyR
 
-d=data.frame(VALUE=0.5, LABEL="None")
+d=data.frame(VALUE=0.5, LABEL=$text)
 
-gp = ggplot() + geom_blank() + geom_text(data=d, mapping=aes(x=VALUE, y=VALUE, label=LABEL), size=10) + theme_void() + theme(legend.position=\"none\");
+gp = ggplot() + geom_blank() + geom_text(data=d, mapping=aes(x=VALUE, y=VALUE, label=LABEL), size=$textSize) + theme_void() + theme(legend.position=\"none\");
 
 plotlist[[plotlist.i]] = gp;
 plotlist.i = plotlist.i + 1;
