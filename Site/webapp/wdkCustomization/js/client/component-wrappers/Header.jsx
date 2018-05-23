8dbf3c07f2d3c16c9c0df6301d1ce63493ee5458
Dave Falke <dmfalke@gmail.com>
2018-05-23 16:25:45 +0000

    add cookie banner
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@89141 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/component-wrappers/Header.jsx
+++ Site/webapp/wdkCustomization/js/client/component-wrappers/Header.jsx
@@ -1,16 +1,23 @@
-import { flow, pick } from 'lodash';
+import { flow } from 'lodash';
+import React from 'react';
 import { withStore, withActions } from '../util/component';
 import { UserActionCreators } from 'wdk-client/ActionCreators';
 import SiteHeader from '../components/SiteHeader';
+import CookieBanner from '../components/CookieBanner';
 
-const withContext = flow(
+const SiteHeaderWithContext = flow(
   withActions(UserActionCreators),
   withStore(state => state.globalData)
-);
+)(SiteHeader);
 
 /**
  * Wrap Header component with state from store and configured actionCreators
  */
 export function Header() {
-  return withContext(SiteHeader)
+  return () => (
+    <React.Fragment>
+      <SiteHeaderWithContext/>
+      <CookieBanner/>
+    </React.Fragment>
+  );
 }
---
+++ Site/webapp/wdkCustomization/js/client/components/CookieBanner.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+// Store user consent in browser storage
+// -------------------------------------
+
+const USER_AGREED_KEY = '@ebrc/agreed-cookies';
+
+const UserAgreedStore = {
+  get: () => JSON.parse(window.localStorage.getItem(USER_AGREED_KEY) || "false"),
+  set: (value) => window.localStorage.setItem(USER_AGREED_KEY, JSON.stringify(Boolean(value)))
+};
+
+
+// Styles
+// ------
+
+const bannerStyle = {
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '2em',
+  fontSize: '1.2em',
+  background: 'black',
+  color: 'white',
+  boxShadow: 'rgb(114, 114, 114) 0px -1px 1px'
+};
+
+const linkStyle = {
+  color: '#96b1e9',
+  textDecoration: 'underline'
+};
+
+
+/**
+ * Inform user of cookie usage. Display banner until user clicks agree button.
+ */
+export default class CookieBanner extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAgreed: undefined,
+      loading: true
+    };
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      userAgreed: UserAgreedStore.get(),
+      loading: false
+    });
+  }
+
+  handleButtonClick() {
+    UserAgreedStore.set(true);
+    this.setState({ userAgreed: true });
+  }
+
+  render() {
+    const { loading, userAgreed } = this.state;
+    return loading || userAgreed ? null : (
+      <div style={bannerStyle}>
+        <div>
+          This website requires cookies, and the limited processing of your
+          personal data in order to function. By using the site you are agreeing
+          to this as outlined in our <a style={linkStyle} target="_blank" href="/documents/EuPathDB_Website_Privacy_Policy.shtml">Privacy Notice</a>.
+        </div>
+        <div>
+          <button style={linkStyle} type="button" onClick={this.handleButtonClick} className="wdk-Link">I agree, dismiss this banner.</button>
+        </div>
+      </div>
+    );
+  }
+}
