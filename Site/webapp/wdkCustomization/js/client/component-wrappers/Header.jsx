dda7d48141c8b99041eaad7d64b9c1f4b2f20198
Dave Falke <dmfalke@gmail.com>
2018-05-24 17:30:36 +0000

    merge from trunk
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/branches/api-build-37@89167 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/component-wrappers/Header.jsx
+++ Site/webapp/wdkCustomization/js/client/component-wrappers/Header.jsx
@@ -1,7 +1,9 @@
 import { flow, pick } from 'lodash';
+import React from 'react';
 import { withStore, withActions } from '../util/component';
 import { UserActionCreators } from 'wdk-client/ActionCreators';
 import SiteHeader from '../components/SiteHeader';
+import CookieBanner from '../components/CookieBanner';
 
 const globalDataItems = [
   'user',
@@ -16,16 +18,21 @@ const globalDataItems = [
   'config'
 ];
 
-const withContext = flow(
+const SiteHeaderWithContext = flow(
   withActions(UserActionCreators),
   withStore(state => {
     return pick(state.globalData, globalDataItems)
   })
-);
+)(SiteHeader);
 
 /**
  * Wrap Header component with state from store and configured actionCreators
  */
 export function Header() {
-  return withContext(SiteHeader)
+  return () => (
+    <React.Fragment>
+      <SiteHeaderWithContext/>
+      <CookieBanner/>
+    </React.Fragment>
+  );
 }
