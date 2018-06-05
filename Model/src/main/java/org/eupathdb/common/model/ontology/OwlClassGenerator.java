ac563fda0aefe3bb6858fcb58ff979121d3a936c
Ryan Doherty <tech@conical.org>
2018-06-05 20:11:33 +0000

    Fix whitespace and slight refactor (no logical changes)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@89375 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/model/ontology/OwlClassGenerator.java
+++ Model/src/main/java/org/eupathdb/common/model/ontology/OwlClassGenerator.java
@@ -1,11 +1,13 @@
 package org.eupathdb.common.model.ontology;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
+import org.apache.log4j.Logger;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -13,7 +15,6 @@ import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
-import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
@@ -22,181 +23,180 @@ import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
 /**
  * Read tab-delimited file and convert to ontology classes associated with some annotationProperties in OWL format
- *  	1st Row is header and contains language setting information of annotations in the format '(language code)', language code is like, en for English, es for Spanish
- *  	2nd Row specifies the AnnotationProperty ID in the ontology
- * 			For annotationProperty ID, it can be in two formats:
- *  		- existing annotationProperty, provide its ID, such as EUPATH_0000274
- *  		- new annotationProperty, using readable label, such as eupath.owl#targetType
- *  	By default, 1st Col is term label, 2nd Col is term parent IRI, and 4th Col and following are annotations of the term
+ *   1st Row is header and contains language setting information of annotations in the format '(language code)' e.g. en for English, es for Spanish
+ *   2nd Row specifies the AnnotationProperty ID in the ontology
+ *     For annotationProperty ID, it can be in two formats:
+ *       - existing annotationProperty, provide its ID, such as EUPATH_0000274
+ *       - new annotationProperty, using readable label, such as eupath.owl#targetType
+ * By default, 1st col is term label, 2nd col is term parent IRI, and 4th col and following are annotations of the term
  *
- *  We need to specify the starting ID need to assign to a new term, if the starting ID is -1, then the label of the term will be used as the ID
+ * We need to specify the starting ID need to assign to a new term, if the starting ID is -1, then the label
+ * of the term will be used as the ID
  *
- *  @author Jie Zheng
+ * @author Jie Zheng
  */
 
 public class OwlClassGenerator {
-	public static void main(String[] args) {
 
-		OwlClassGeneratorOptions bean = new OwlClassGeneratorOptions();
-	    CmdLineParser parser = new CmdLineParser(bean);
+  private static final Logger LOG = Logger.getLogger(OwlClassGenerator.class);
 
-	    try {
-	        parser.parseArgument(args);
-	    } catch( CmdLineException e ) {
-	        parser.printUsage(System.err);
-                //	        System.err.println(e.getMessage());
-	        System.exit(1);
-	    }
+  public static void main(String[] args) {
+    try {
+      new OwlClassGenerator().generateOwlFile(parseArgs(args));
+    }
+    catch (Exception e) {
+      System.err.println(e.getMessage());
+    }
+  }
 
-		String path = bean.getPath();
-		String inputFilename = bean.getInputFilename();
-		String idBase = bean.getIdBase();
-		String domainName = bean.getDomainName();
-		String outputFilename = bean.getOutputFilename();
-		String ontoIRIstr = bean.getOntoIRIstr();
-		int labelPos = bean.getLabelPos();
-		int parentPos = bean.getParentPos();
-		int annotPos = bean.getAnnotPos();
-		int startId = bean.getStartId();
+  public void generateOwlFile(OwlClassGeneratorOptions config) throws IOException {
 
-		// load tab-delimited file
-		BufferedReader br = null;
-		ArrayList <String[]> matrix = new ArrayList <String[]> ();
+    // load tab-delimited file
+    List<String[]> matrix = loadIndividualsFile(config);
 
-		try {
-			br = new BufferedReader(new FileReader(path + inputFilename));
+    // create an OWL file
+    OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+    OWLOntology outOWL = OntologyManipulator.create(manager, config.getOntoIRIstr());
 
-			String line = null;
-			int lineNum = 0;
-			while( (line = br.readLine()) != null)
-			{
-			    lineNum ++;
-				if (!(line.trim().startsWith("##") || line.trim().length()==0)) {
-					String[] items = line.split("\t");
-					matrix.add(items);
-				
-					if (items.length > matrix.get(0).length) {
-					    System.out.println("Generation of the OWl file, " + outputFilename + ", stop because input tab-delimited file line " + lineNum + " has extra columns");
-					    System.out.println("\t" + inputFilename + " Line " + lineNum + " has " + items.length + " columns and required columns number is " + matrix.get(0).length);
-					    System.exit(0);
-					}
-				}
-			}
-			System.out.println("Successfully read text file: " + inputFilename);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if ( br != null ) br.close();
-			}
-			catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
+    // Create factory to obtain a reference to a class
+    OWLDataFactory df = manager.getOWLDataFactory();
 
-    	// create an OWL file
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLOntology outOWL = OntologyManipulator.create(manager, ontoIRIstr);
+    // Find annotation language information based on the column header in the tab-delimited file (1st row)
+    String[] languageCodes = loadLanguageCodes(matrix.get(0), config.getAnnotPos());
 
-        // Create factory to obtain a reference to a class
-        OWLDataFactory df = manager.getOWLDataFactory();
+    // Create annotation properties based on the IDs provided in the tab-delimited file (2nd row)
+    List<OWLAnnotationProperty> annotProps = loadAnnotationProps(matrix.get(1), config.getAnnotPos(), config.getIdBase(), df);
 
-        // Find annotation language information based on the column header in the tab-delimited file (1st row)		
-        String[] items = matrix.get(0);
-		String[] languageCodes = new String[items.length];
-		
-        for (int k = annotPos; k< items.length; k++) {
-			int start = items[k].indexOf("(");
-			int end = items[k].indexOf(")");
-			if (start >= 0 && end >=0) {
-				languageCodes[k] = items[k].substring(start+1, end);
-			} else {
-				languageCodes[k] = "";
-			}
-		}
-        
-        // Create annotation properties based on the IDs provided in the tab-delimited file on the 2nd row
-        ArrayList<OWLAnnotationProperty> annotProps = new ArrayList<OWLAnnotationProperty>();
-        items = matrix.get(1);
-        for (int k = annotPos; k< items.length; k++) {
-        	String item = cleanString(items[k]);
-        	if (item.length() > 0) {
-        		OWLAnnotationProperty annotProp = df.getOWLAnnotationProperty(IRI.create(idBase +item));
-        		annotProps.add(annotProp);
-        	}
+    // create classes
+    createClasses(config, matrix, manager, outOWL, df, languageCodes, annotProps);
+
+    // save file to disk
+    OntologyManipulator.saveToFile(manager, outOWL, config.getOutputFilePath());
+  }
+
+  private static void createClasses(OwlClassGeneratorOptions config, List<String[]> matrix, OWLOntologyManager manager,
+      OWLOntology outOWL, OWLDataFactory df, String[] languageCodes, List<OWLAnnotationProperty> annotProps) {
+    Function<String,String> iriDecorator = str -> config.getOntoIRIstr() + "#" + str;
+    int startId = config.getStartId();
+    for (int i = 2; i < matrix.size(); i++) {
+      String[] tokens = matrix.get(i);
+
+      // generate class IRI
+      String termIRIstr;
+      if (startId == -1) {
+        termIRIstr = iriDecorator.apply(cleanString(tokens[config.getLabelPos()]));
+      }
+      else {
+        termIRIstr = config.getIdBase() + getOntologyTermID(config.getDomainName(), startId);
+        startId++;
+      }
+
+      OWLClass cls = df.getOWLClass(IRI.create(termIRIstr));
+
+      // add class parent
+      OWLClass parent = getParentClass(tokens[config.getParentPos()], iriDecorator, df);
+      manager.applyChange(new AddAxiom(outOWL, df.getOWLSubClassOfAxiom(cls, parent)));
+
+      // add term label
+      OWLAnnotationProperty labelProp = df.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_LABEL.getIRI());
+      OWLAnnotation label = df.getOWLAnnotation(labelProp, df.getOWLLiteral(cleanString(tokens[config.getLabelPos()])));
+      manager.applyChange(new AddAxiom(outOWL, df.getOWLAnnotationAssertionAxiom(cls.getIRI(), label)));
+
+      // add other annotation properties
+      for (int j = config.getAnnotPos(); j < tokens.length; j++) {
+        String item = cleanString(tokens[j]);
+        int jOffset = j - config.getAnnotPos();
+        if (!item.isEmpty() && annotProps.size() > jOffset) {
+          String lang = languageCodes[j];
+          OWLAnnotationProperty property = annotProps.get(jOffset);
+          OWLAnnotation annot = lang.isEmpty() ?
+            df.getOWLAnnotation(property, df.getOWLLiteral(item)) :
+            df.getOWLAnnotation(property, df.getOWLLiteral(item, lang));
+          manager.applyChange(new AddAxiom(outOWL, df.getOWLAnnotationAssertionAxiom(cls.getIRI(), annot)));
         }
+      }
+    }
+  }
 
-        // create classes
-	   	for (int i = 2; i < matrix.size(); i++) {
-	   		items = matrix.get(i);
-	   		
-	   		// generate class IRI
-	   		String termIRIstr;
+  private static OWLClass getParentClass(String parentToken, Function<String,String> iriDecorator, OWLDataFactory df) {
+    String parentStr = cleanString(parentToken);
+    String iriStr = 
+        parentStr.startsWith("http") ? parentStr :
+        parentStr.length() > 0 ? iriDecorator.apply(parentStr) :
+        "http://www.w3.org/2002/07/owl#Thing";
+    return df.getOWLClass(IRI.create(iriStr));
+  }
 
-	   		if (startId == -1) {
-	   			termIRIstr = ontoIRIstr + "#" +  cleanString(items[labelPos]);
-	   		} else {
-	   			termIRIstr = idBase + getOntologyTermID(domainName,startId);
-	   			startId ++;
-	   		}
+  private static List<OWLAnnotationProperty> loadAnnotationProps(String[] items, int annotPos, String idBase, OWLDataFactory df) {
+    List<OWLAnnotationProperty> annotProps = new ArrayList<>();
+    for (int k = annotPos; k < items.length; k++) {
+      String item = cleanString(items[k]);
+      if (item.length() > 0) {
+        OWLAnnotationProperty annotProp = df.getOWLAnnotationProperty(IRI.create(idBase + item));
+        annotProps.add(annotProp);
+      }
+    }
+    return annotProps;
+  }
 
-	   		OWLClass cls = df.getOWLClass(IRI.create(termIRIstr));
+  private static String[] loadLanguageCodes(String[] tokens, int annotPos) {
+    String[] languageCodes = new String[tokens.length];
+    for (int k = annotPos; k < tokens.length; k++) {
+      int start = tokens[k].indexOf("(");
+      int end = tokens[k].indexOf(")");
+      if (start >= 0 && end >= 0) {
+        languageCodes[k] = tokens[k].substring(start + 1, end);
+      }
+      else {
+        languageCodes[k] = "";
+      }
+    }
+    return languageCodes;
+  }
 
-	   		// add class parent
-	   		OWLClass parent = null;
-	   		String parentStr = cleanString(items[parentPos]);
-	   		if (parentStr.startsWith("http")) {
-	   			parent = df.getOWLClass(IRI.create(parentStr));
-	   		} else if (parentStr.length() > 0){
-	   			parent = df.getOWLClass(IRI.create(ontoIRIstr + "#" + parentStr));
-	   		} else {
-	   			parent = df.getOWLClass(IRI.create("http://www.w3.org/2002/07/owl#Thing"));
-	   		}
-	   		
-	   		OWLAxiom axiom = df.getOWLSubClassOfAxiom(cls, parent);
-	   		manager.applyChange(new AddAxiom(outOWL, axiom));
+  private static List<String[]> loadIndividualsFile(OwlClassGeneratorOptions config) throws IOException {
+    try (BufferedReader br = new BufferedReader(new FileReader(config.getInputFilePath()))) {
+      List<String[]> matrix = new ArrayList<>();
+      String line = null;
+      int lineNum = 0;
+      while ((line = br.readLine()) != null) {
+        lineNum++;
+        if (!(line.trim().startsWith("##") || line.trim().length() == 0)) {
+          String[] items = line.split("\t");
+          matrix.add(items);
+          if (items.length > matrix.get(0).length) {
+            throw new IOException("Generation of the OWL file, " + config.getOutputFilename() + "has stopped.  Line " +
+                lineNum + " in input tab-delimited file " + config.getInputFilename() + " has " + items.length +
+                " columns but " + matrix.get(0).length + " are expected.");
+          }
+        }
+      }
+      LOG.info("Successfully read text file: " + config.getInputFilename());
+      return matrix;
+    }
+  }
 
-	        // add term label
-	        OWLAnnotationProperty labelProp = df.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_LABEL.getIRI());
-	        OWLAnnotation label = df.getOWLAnnotation(labelProp, df.getOWLLiteral(cleanString(items[labelPos])));
-	        axiom = df.getOWLAnnotationAssertionAxiom(cls.getIRI(), label);
-        	manager.applyChange(new AddAxiom(outOWL, axiom));
-        	//System.out.println(termIRIstr + "\t" + items[labelPos].trim());
+  private static OwlClassGeneratorOptions parseArgs(String[] args) {
+    OwlClassGeneratorOptions config = new OwlClassGeneratorOptions();
+    CmdLineParser parser = new CmdLineParser(config);
+    try {
+      parser.parseArgument(args);
+      return config;
+    }
+    catch (CmdLineException e) {
+      parser.printUsage(System.err);
+      System.exit(1);
+      return null;
+    }
+  }
 
-	        // add other annotation properties
-	        for (int j = annotPos; j < items.length; j ++) {
-	        	String item = cleanString(items[j]);
-	        	if (item.length()>0 && annotProps.size() > j-annotPos) {
-	        		OWLAnnotation annot = null;
-    	        	String lang = languageCodes[j];
-	        		
-    	        	if (lang.length() == 0) {
-    	        		annot = df.getOWLAnnotation(annotProps.get(j-annotPos), df.getOWLLiteral(item));
-    	        	} else {
-    	        		annot = df.getOWLAnnotation(annotProps.get(j-annotPos), df.getOWLLiteral(item, lang));
-    	        	}
-    	        	
-	        		axiom = df.getOWLAnnotationAssertionAxiom(cls.getIRI(), annot);
-	        		manager.applyChange(new AddAxiom(outOWL, axiom));
-	        	}
-	        }
-	   	}
+  private static String getOntologyTermID(String domainName, int startID) {
+    String s = "0000000" + startID;
+    String id = domainName.toUpperCase() + "_" + s.substring(s.length() - 7);
+    return id;
+  }
 
-	   	OntologyManipulator.saveToFile(manager, outOWL, path + outputFilename);
-	}
-
-	public static String getOntologyTermID (String domainName, int startID) {
-		String s = "0000000" + startID;
-		String id = domainName.toUpperCase() + "_" + s.substring(s.length()-7);
-
-		return id;
-	}
-	
-	public static String cleanString (String s) {
-		s = s.trim().replaceAll("^\"|\"$", "");	
-
-		return s;
-	}
+  private static String cleanString(String s) {
+    return s.trim().replaceAll("^\"|\"$", "");
+  }
 }
--- Model/src/main/java/org/eupathdb/common/model/ontology/OwlClassGeneratorOptions.java
+++ Model/src/main/java/org/eupathdb/common/model/ontology/OwlClassGeneratorOptions.java
@@ -3,82 +3,86 @@ package org.eupathdb.common.model.ontology;
 import org.kohsuke.args4j.Option;
 
 /**
- *  Process arguments passed from command line for OntologyMerger.java
+ * Process arguments passed from command line for OntologyMerger.java
  *
- *  @author Jie Zheng
+ * @author Jie Zheng
  */
 public class OwlClassGeneratorOptions {
-    @Option(name="-path", usage ="the directory contains input file and also used for saved output file", required = false)
-        private String path = "/var/www/PlasmoDB/plasmo.jbrestel/gus_home/lib/wdk/ontology/";
 
-    @Option(name="-inputFilename", usage="input tab-delimited file that will be used to convert to OWL format file", required = false)
-    private String inputFilename = "individuals.txt";
+  @Option(name = "-path", usage = "the directory contains input file and also used for saved output file", required = false)
+  private String path = "/var/www/PlasmoDB/plasmo.jbrestel/gus_home/lib/wdk/ontology/";
 
-    @Option(name="-labelPos", usage="number of column in the tab delimited file that contains term labels ", required = false)
-    private String labelPos = "1";
+  @Option(name = "-inputFilename", usage = "input tab-delimited file that will be used to convert to OWL format file", required = false)
+  private String inputFilename = "individuals.txt";
 
-    @Option(name="-parentPos", usage="number of column in the tab delimited file that contains term parent IRIs", required = false)
-    private String parentPos = "2";
+  @Option(name = "-labelPos", usage = "number of column in the tab delimited file that contains term labels ", required = false)
+  private String labelPos = "1";
 
-    @Option(name="-annotPos", usage="number of column and following colums in the tab delimited file that contain annotation of terms", required = false)
-    private String annotPos = "4";
+  @Option(name = "-parentPos", usage = "number of column in the tab delimited file that contains term parent IRIs", required = false)
+  private String parentPos = "2";
 
-    @Option(name="-idBase", usage="Ontology term URI base", required = false)
-    private String idBase = "http://purl.obolibrary.org/obo/";
+  @Option(name = "-annotPos", usage = "number of column and following colums in the tab delimited file that contain annotation of terms", required = false)
+  private String annotPos = "4";
 
-    @Option(name="-domainName", usage="used to create IRI for newly added terms"
-    		+ "", required = false)
-    private String domainName = "EUPATH";
+  @Option(name = "-idBase", usage = "Ontology term URI base", required = false)
+  private String idBase = "http://purl.obolibrary.org/obo/";
 
-    @Option(name="-startId", usage="unique ID assigned to the newly added terms when startId >0, otherwise, the term label will be used", required = false)
-    private String startId = "-1";
+  @Option(name = "-domainName", usage = "used to create IRI for newly added terms", required = false)
+  private String domainName = "EUPATH";
 
-    @Option(name="-ontoIRIstr", usage="IRI of ontology converted from the tab-delimited file", required = false)
-    private String ontoIRIstr = "http://purl.obolibrary.org/obo/eupath/individuals.owl";
+  @Option(name = "-startId", usage = "unique ID assigned to the newly added terms when startId >0, otherwise, the term label will be used", required = false)
+  private String startId = "-1";
 
-    @Option(name="-outputFilename", usage="The filename of output OWL file", required = false)
-    private String outputFilename = "individuals.owl";
+  @Option(name = "-ontoIRIstr", usage = "IRI of ontology converted from the tab-delimited file", required = false)
+  private String ontoIRIstr = "http://purl.obolibrary.org/obo/eupath/individuals.owl";
 
-    public String getPath () {
-    	return this.path;
-    }
+  @Option(name = "-outputFilename", usage = "The filename of output OWL file", required = false)
+  private String outputFilename = "individuals.owl";
 
-    public String getInputFilename () {
-    	return this.inputFilename;
-    }
+  public String getInputFilename() {
+    return inputFilename;
+  }
 
-    public String getIdBase () {
-    	return this.idBase;
-    }
+  public String getInputFilePath() {
+    return path + inputFilename;
+  }
 
-    public String getDomainName () {
-    	return this.domainName;
-    }
+  public String getOutputFilename() {
+    return outputFilename;
+  }
 
-    public int getLabelPos () {
-    	int pos = Integer.parseInt(this.labelPos) -1;
-    	return pos;
-    }
+  public String getOutputFilePath() {
+    return path + outputFilename;
+  }
 
-    public int getParentPos () {
-    	int pos = Integer.parseInt(this.parentPos) -1;
-    	return pos;
-    }
+  public String getIdBase() {
+    return idBase;
+  }
 
-    public int getAnnotPos () {
-    	int pos = Integer.parseInt(this.annotPos) -1;
-    	return pos;
-    }
+  public String getDomainName() {
+    return domainName;
+  }
 
-    public int getStartId () {
-    	return Integer.parseInt(this.startId);
-    }
+  public int getLabelPos() {
+    int pos = Integer.parseInt(labelPos) - 1;
+    return pos;
+  }
 
-    public String getOutputFilename () {
-    	return this.outputFilename;
-    }
+  public int getParentPos() {
+    int pos = Integer.parseInt(parentPos) - 1;
+    return pos;
+  }
 
-    public String getOntoIRIstr () {
-    	return this.ontoIRIstr;
-    }
+  public int getAnnotPos() {
+    int pos = Integer.parseInt(annotPos) - 1;
+    return pos;
+  }
+
+  public int getStartId() {
+    return Integer.parseInt(startId);
+  }
+
+  public String getOntoIRIstr() {
+    return ontoIRIstr;
+  }
 }
