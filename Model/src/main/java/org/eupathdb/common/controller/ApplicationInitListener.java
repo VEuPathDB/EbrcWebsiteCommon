0106f91e80ff393ddaffbfb6f26a2383bbe9962b
Ryan Doherty <tech@conical.org>
2018-07-16 20:49:22 +0000

    Add generic init listener for ebrc sites
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@90132 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/eupathdb/common/controller/ApplicationInitListener.java
@@ -0,0 +1,30 @@
+package org.eupathdb.common.controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.gusdb.wdk.controller.WdkInitializer;
+import org.gusdb.wdk.model.WdkModel;
+
+/**
+ * A class that is initialized at the start of the web application. This makes
+ * sure global resources are available to all the contexts that need them.
+ */
+public class ApplicationInitListener implements ServletContextListener {
+
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    ServletContext context = sce.getServletContext();
+    WdkInitializer.initializeWdk(context);
+    WdkModel wdkModel = WdkInitializer.getWdkModel(context);
+    EuPathSiteSetup.initialize(wdkModel);
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    ServletContext context = sce.getServletContext();
+    WdkInitializer.terminateWdk(context);
+  }
+}
+
