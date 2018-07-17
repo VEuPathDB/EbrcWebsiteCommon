c3d7343e35b5916d6e74688b28a925217253d081
Danielle Callan <dcallan@upenn.edu>
2018-07-17 17:28:56 +0000

    legend for prism malaria timeline plots
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@90172 735e2a04-f8fc-0310-8a1b-f2942603c481

--- View/lib/perl/CgiApp/DataPlotter.pm
+++ View/lib/perl/CgiApp/DataPlotter.pm
@@ -227,11 +227,10 @@ sub run {
 
 	 # report or delete temporary files.
 	 if ($save_b) {
-      print STDERR join("\n", 'Files', @filesToDelete), "\n";
-	 }
-   else {
-	  map {unlink $_ if $_} @filesToDelete;
-   }
+           print STDERR join("\n", 'Files', @filesToDelete), "\n";
+	 } else {
+	   map {unlink $_ if $_} @filesToDelete;
+         }
 }
 
 # ========================================================================
