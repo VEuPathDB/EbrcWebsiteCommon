22a3e8645d99d7588ca4aec3cb1676ad7c58326f
Ryan Doherty <tech@conical.org>
2018-10-30 14:23:24 +0000

    Move AnswerValue back to original location to ease merging
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/branches/strategy-loading@92971 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/model/report/summaryview/BlastViewReporter.java
+++ Model/src/main/java/org/eupathdb/common/model/report/summaryview/BlastViewReporter.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import org.gusdb.fgputil.json.JsonWriter;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
-import org.gusdb.wdk.model.answer.factory.AnswerValue;
+import org.gusdb.wdk.model.answer.AnswerValue;
 import org.gusdb.wdk.model.report.AbstractReporter;
 import org.gusdb.wdk.model.report.Reporter;
 import org.gusdb.wdk.model.report.ReporterConfigException;
