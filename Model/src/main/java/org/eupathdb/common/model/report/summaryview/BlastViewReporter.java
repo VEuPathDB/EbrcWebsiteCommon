b8d1c5f9e682b9ed467d57f78617d0a712620443
Steve Fischer <sfischer@upenn.edu>
2018-11-02 17:32:17 +0000

    revert to extending DefaultJsonReporter
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/trunk@93030 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/eupathdb/common/model/report/summaryview/BlastViewReporter.java
+++ Model/src/main/java/org/eupathdb/common/model/report/summaryview/BlastViewReporter.java
@@ -1,26 +1,15 @@
 package org.eupathdb.common.model.report.summaryview;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.Map;
-
 import org.gusdb.fgputil.json.JsonWriter;
 import org.gusdb.wdk.model.WdkModelException;
-import org.gusdb.wdk.model.WdkUserException;
 import org.gusdb.wdk.model.answer.AnswerValue;
-import org.gusdb.wdk.model.report.AbstractReporter;
 import org.gusdb.wdk.model.report.Reporter;
 import org.gusdb.wdk.model.report.ReporterConfigException;
+import org.gusdb.wdk.model.report.reporter.DefaultJsonReporter;
 import org.json.JSONObject;
-import org.apache.log4j.Logger;
 
-public class BlastViewReporter extends AbstractReporter {
-
-  @SuppressWarnings("unused")
-  private static final Logger LOG = Logger.getLogger(BlastViewReporter.class);
-
+public class BlastViewReporter extends DefaultJsonReporter {
+  
   public static final String ATTR_HEADER = "blastHeader";
   public static final String ATTR_MIDDLE = "blastMiddle";
   public static final String ATTR_FOOTER = "blastFooter";
@@ -33,45 +22,29 @@ public class BlastViewReporter extends AbstractReporter {
   public BlastViewReporter(AnswerValue answerValue) {
     super(answerValue);
   }
-
+  
   @Override
   public Reporter configure(JSONObject config) throws ReporterConfigException, WdkModelException {
     if (!_baseAnswer.getQuestion().getName().matches("GenesBySimilarity") ) 
       throw new ReporterConfigException("Only BLAST searches can use this report");
-    return this;
+    return super.configure(config);
   }
 
   @Override
-  public Reporter configure(Map<String, String> config) throws ReporterConfigException, WdkModelException {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  protected void write(OutputStream out) throws WdkModelException {
-
-    // create output writer and initialize record stream
-    try (JsonWriter writer = new JsonWriter(new BufferedWriter(new OutputStreamWriter(out)))) {
-      JSONObject json = new JSONObject();
-      String message = _baseAnswer.getResultMessage();
-      String[] pieces = message.split(MACRO_SUMMARY, 2);
-      json.put(ATTR_HEADER, pieces[0]);
-      if (pieces.length > 1) {
-        pieces = pieces[1].split(MACRO_ALIGNMENT, 2);
-        json.put(ATTR_MIDDLE, pieces[0]);
-        if (pieces.length > 1)
-          json.put(ATTR_FOOTER, pieces[1]);
-      }
-      writer.object();
-      writer.key(BLAST_META).value(json);
-      writer.endObject();
+  public JsonWriter writeAdditionalJson(JsonWriter writer) { 
+    
+    JSONObject json = new JSONObject();
+    String message = _baseAnswer.getResultMessage();
+    String[] pieces = message.split(MACRO_SUMMARY, 2);
+    json.put(ATTR_HEADER, pieces[0]);
+    if (pieces.length > 1) {
+      pieces = pieces[1].split(MACRO_ALIGNMENT, 2);
+      json.put(ATTR_MIDDLE, pieces[0]);
+      if (pieces.length > 1)
+        json.put(ATTR_FOOTER, pieces[1]);
     }
-    catch (WdkUserException | IOException e) {
-      throw new WdkModelException("Unable to write reporter result to output stream", e);
-    }   
+    writer.key(BLAST_META).value(json);
+    return writer; 
   }
-
-  @Override
-  public String getHttpContentType() {
-    return "application/json";
-  }
+  
 }
