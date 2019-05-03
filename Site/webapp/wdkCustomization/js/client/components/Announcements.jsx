2536388a9860eca934ed37c33c8502a4d9211bf4
Cristina Aurrecoechea <aurreco@uga.edu>
2019-05-03 15:46:39 +0000

    banner for cecomm migration
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/branches/api-build-43@97933 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
+++ Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
@@ -87,18 +87,19 @@ const siteAnnouncements = [
   },
 */
  // Clinepi home page
-/*
+
   (props) => {
-    if ( (props.projectId == 'Gates' || props.projectId == 'ICEMR' || props.projectId == 'ClinEpiDB') && (location.pathname == props.webAppUrl + '/app') ) {
+    if ( (props.projectId == 'Gates' || props.projectId == 'ICEMR' || props.projectId == 'ClinEpiDB') && 
+         (location.pathname == props.webAppUrl + '/app') || (location.pathname == props.webAppUrl + '/app/') ) {
       return (
-        <div key="clinepi-astmh">
-          The EuPathDB and ClinEpiDB team will be attending the <a target='_blank' href='https://www.astmh.org/annual-meeting'>American Society of Tropical Medicine and Hygiene annual meeting</a> next week Sunday, October 28th to Thursday, November 1st! Stop by to see us in the exhibition hall booth 317 & 319 and at Tuesday's Poster Session B  #809.
+        <div key="clinepi-dbUpgrade">
+          Due to planned upgrades, you may experience issues saving strategies today from 11:00 EDT - 20:00 EDT 
         </div>
       );
     }
     return null;
   },
-*/
+
 
 // Fungi gene page for Cryptococcus neoformans KN99
 /*  (props) => { 
