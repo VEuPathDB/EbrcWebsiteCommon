1f8860187aa29951e46fa8e72e6e826a66777c85
Dave Falke <dmfalke@gmail.com>
2019-06-07 14:01:03 +0000

    update to use new interfaces
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcWebsiteCommon/branches/strategy-loading@98727 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Site/webapp/wdkCustomization/js/client/wrapWdkService.js
+++ Site/webapp/wdkCustomization/js/client/wrapWdkService.js
@@ -1,22 +1,15 @@
 import { ok } from 'wdk-client/Utils/Json';
 
-export default WdkService => class EbrcWdkService extends WdkService {
-
-  getStudies(attributes) {
-    return this.sendRequest(ok, {
-      useCache: 'true',
-      cacheId: 'studies',
-      method: 'post',
-      path: this.getAnswerJsonServicePath(),
-      body: JSON.stringify({
-        answerSpec: {
-          filters: [],
-          parameters: {},
-          questionName: 'DatasetQuestions.AllDatasets'
-        },
-        formatConfig: { attributes }
-      })
+export default wdkService => ({
+  ...wdkService,
+  getStudies: attributes => wdkService.sendRequest(ok, {
+    useCache: 'true',
+    cacheId: 'studies',
+    method: 'post',
+    path: wdkService.getStandardSearchReportEndpoint('dataset', 'AllDatasets'),
+    body: JSON.stringify({
+      searchConfig: { parameters: {} },
+      reportConfig: { attributes }
     })
-  }
-
-}
+  })
+});
