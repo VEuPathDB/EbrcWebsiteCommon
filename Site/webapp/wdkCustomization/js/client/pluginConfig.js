523ce095c0480322771e8813ac0daa3611648ca3
Jamie Long <jtlong3rd@gmail.com>
2019-10-24 14:31:30 -0400

    Add Ebrc step analysis plugins to the ClientPlugin registry

--- Site/webapp/wdkCustomization/js/client/pluginConfig.js
+++ Site/webapp/wdkCustomization/js/client/pluginConfig.js
@@ -5,6 +5,7 @@ import {
   ResultTableSummaryViewPlugin, 
   StepAnalysisDefaultForm,
   StepAnalysisDefaultResult,
+  StepAnalysisEupathExternalResult,
 } from 'wdk-client/Plugins';
 
 export default [
@@ -31,6 +32,56 @@ export default [
   {
     type: 'stepAnalysisForm',
     component: StepAnalysisDefaultForm
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'otu_abundance',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'alpha_diversity',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'beta_diversity',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'differential_abundance',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'correlation_app',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'person-graph-analysis',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'light-trap-plots',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-cont-table',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-event-dist',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-summaries',
+    component: StepAnalysisEupathExternalResult
   },
   {
     type: 'stepAnalysisResult',
