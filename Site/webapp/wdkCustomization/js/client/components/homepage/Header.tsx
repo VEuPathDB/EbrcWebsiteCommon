04ef91ce04383357931840925f355f8778f53257
Jamie Long <jtlong3rd@gmail.com>
2019-10-31 09:34:13 -0400

    Initial/checkpoint commit of Header content

--- Site/webapp/wdkCustomization/js/client/components/homepage/Header.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/Header.tsx
@@ -1,20 +1,253 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 
+import { Link, TextBox, IconAlt } from 'wdk-client/Components';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
+
+import { webAppUrl } from '../../config';
+
+import { combineClassNames } from './Utils';
 
 import './Header.scss';
 
 const cx = makeClassNameHelper('ebrc-Header');
 
+const useWebAppUrl = (): string => {
+  return webAppUrl;
+};
+
 type Props = {
-  containerClassName?: string;
+  branding: ReactNode,
+  containerClassName?: string,
+  loadSuggestions: (searchTerm: string) => void,
+  menuItems: HeaderMenuItem[],
+  siteSearchSuggestions?: string[],
+  additionalSuggestions?: AdditionalSuggestionItem[]
+};
+
+type HeaderMenuItemBase = {
+  key: string,
+  display: ReactNode,
+  tooltip?: string
+};
+
+interface RouteMenuItem extends HeaderMenuItemBase {
+  type: 'route';
+  route: string;
+  target?: string;
 }
 
-export const Header = ({ containerClassName }: Props) => {
-  const className = containerClassName ? `${cx()} ${containerClassName}` : cx();
+interface WebAppMenuItem extends HeaderMenuItemBase {
+  type: 'webAppRoute';
+  urlSegment: string;
+  target?: string;
+}
+
+interface ExternalLinkMenuItem extends HeaderMenuItemBase {
+  type: 'externalLink',
+  href: string
+  target?: string
+}
+
+interface SubmenuItem extends HeaderMenuItemBase {
+  type: 'subMenu',
+  items: HeaderMenuItem[]
+}
+
+export type HeaderMenuItem = 
+  | RouteMenuItem
+  | WebAppMenuItem
+  | ExternalLinkMenuItem
+  | SubmenuItem;
+
+type AdditionalSuggestionItem = {
+  key: string,
+  display: ReactNode
+};
+
+export const Header = ({ 
+  branding, 
+  containerClassName, 
+  loadSuggestions, 
+  menuItems,
+  siteSearchSuggestions,
+  additionalSuggestions
+}: Props) => {
+  const [ selectedMenuItems, setSelectedMenuItems ] = useState<string[]>([]);
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const [ isSearchBarSelected, setIsSearchBarSelected ] = useState(false);
+  const [ isSearchBarToggleHidden, setIsSearchBarToggleHidden ] = useState(true);
+
+  console.log(selectedMenuItems);
+
+  useEffect(() => {
+    loadSuggestions(searchTerm);
+  }, [ searchTerm ]);
 
   return (
-    <header className={className}>
+    <header className={combineClassNames(cx(), containerClassName)}>
+      {branding}
+      <MenuItemGroup
+        menuItems={menuItems}
+        selectedItems={selectedMenuItems}
+        setSelectedItems={setSelectedMenuItems}
+      />
+      <div className={cx('SearchBar', isSearchBarToggleHidden ? 'toggle-hidden' : 'toggle-shown')}>
+        <TextBox 
+          onChange={setSearchTerm}
+          value={searchTerm}
+          placeholder="Site search..."
+          onFocus={() => {
+            setIsSearchBarSelected(true);
+          }}
+          onBlur={() => {
+            setIsSearchBarSelected(false);
+          }}
+        />
+        <div className={cx('SearchSubmit')}>
+          <IconAlt fa="search" />
+        </div>
+        {
+          isSearchBarSelected && siteSearchSuggestions && additionalSuggestions &&
+          <Suggestions
+            siteSearchSuggestions={siteSearchSuggestions}
+            additionalSuggestions={additionalSuggestions}
+          />
+        }
+      </div>
+      <div className={cx('SearchBarToggle', isSearchBarToggleHidden ? 'hidden' : 'shown')}>
+        <div className={cx('SearchSubmit')}>
+          <IconAlt fa="search" />
+        </div>
+      </div>
     </header>
   );
 };
+
+type MenuItemGroupProps = {
+  menuItems: HeaderMenuItem[],
+  path?: string[],
+  selectedItems: string[],
+  setSelectedItems: (newSelectedItems: string[]) => void
+};
+
+const MenuItemGroup = ({
+  menuItems,
+  path = [],
+  selectedItems,
+  setSelectedItems
+}: MenuItemGroupProps) => 
+  <div className={cx('MenuItemGroup')}>
+    {
+      menuItems.map(
+        menuItem => 
+          <HeaderMenuItemContent
+            key={menuItem.key}
+            item={menuItem}
+            path={[...path, menuItem.key]}
+            selectedItems={selectedItems}
+            setSelectedItems={setSelectedItems}
+          />
+      )
+    }
+  </div>;
+
+type HeaderMenuItemContentProps = {
+  item: HeaderMenuItem
+  path: string[],
+  selectedItems: string[],
+  setSelectedItems: (newSelectedItems: string[]) => void
+};
+
+const HeaderMenuItemContent = ({
+  item,
+  path,
+  selectedItems,
+  setSelectedItems
+}: HeaderMenuItemContentProps) => {
+  const webAppUrl = useWebAppUrl();
+
+  return (
+    <div className={cx('MenuItemContent')}>
+      {
+        item.type === 'route'
+          ? <Link 
+              to={item.route} 
+              target={item.target}
+            >
+              {item.display}
+            </Link>
+          : item.type === 'webAppRoute'
+          ? <a 
+              href={`${webAppUrl}${item.urlSegment}`} 
+              target={item.target}
+            >
+              {item.display}
+            </a>
+          : item.type === 'externalLink'
+          ? <a 
+              href={item.href} 
+              target={item.target}
+            >
+              {item.display}
+            </a>
+          : <div className={cx('SubmenuGroup')}>
+              <button type="button"
+                onBlur={(e) => {
+                  e.stopPropagation();
+                  setSelectedItems(path.slice(0, -1));
+                }}
+                onFocus={(e) => {
+                  e.stopPropagation();
+                  setSelectedItems(path);
+                }}
+                onMouseEnter={(e) => {
+                  e.stopPropagation();
+                  setSelectedItems(path);
+                }}
+                onMouseLeave={(e) => {
+                  e.stopPropagation();
+                  setSelectedItems(path.slice(0, -1));
+                }}
+              >
+                {item.display}
+                <IconAlt fa="caret-down" />
+              </button>
+              <MenuItemGroup 
+                menuItems={item.items} 
+                path={path}
+                selectedItems={selectedItems}
+                setSelectedItems={setSelectedItems}
+              />
+            </div>
+      }
+    </div>
+  );
+};
+
+type SuggestionsProps = {
+  siteSearchSuggestions: string[],
+  additionalSuggestions: AdditionalSuggestionItem[]
+};
+
+const Suggestions = ({
+  siteSearchSuggestions,
+  additionalSuggestions
+}: SuggestionsProps) => 
+  <div className={cx('Suggestions')}>
+    {
+      siteSearchSuggestions.map(
+        siteSearchSuggestion => 
+          <div key={siteSearchSuggestion} className={cx('SiteSearchSuggestion')}>
+            <IconAlt fa="search" />{siteSearchSuggestion}
+          </div>
+      )
+    }
+    {
+      additionalSuggestions && additionalSuggestions.length > 0 && additionalSuggestions.map(
+        additionalSuggestion =>
+        <div key={additionalSuggestion.key} className={cx('AdditionalSuggestion')}>
+          {additionalSuggestion.display}
+        </div>
+      )
+    }
+  </div>;
