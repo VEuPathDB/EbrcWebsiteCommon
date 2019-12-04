d7dd6e3da85caf9ca3059f29c88a31506e767e4f
Dave Falke <dmfalke@gmail.com>
2019-12-03 22:36:12 -0500

    Add missing imports

--- Site/webapp/wdkCustomization/js/client/components/GalaxyPageLayout.jsx
+++ Site/webapp/wdkCustomization/js/client/components/GalaxyPageLayout.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function GalaxyPageLayout(props) {
   return (
     <div id="eupathdb-GalaxyTerms">
--- Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
+++ Site/webapp/wdkCustomization/js/client/components/GalaxyTerms.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 import { Link } from 'wdk-client/Components';
 import { projectId } from '../config';
 import GalaxyPageLayout from './GalaxyPageLayout';
