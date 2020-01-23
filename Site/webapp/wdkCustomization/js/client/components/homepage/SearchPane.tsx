dfc1bd6fa659254ab347ff9e5b7d1b09a51d2e8f
Jamie Long <jtlong3rd@gmail.com>
2020-01-23 10:56:57 -0500

    Last-minute tweaks to Search Pane (order by record types and categories; unbolden subcategories; change header; darken wrench)

--- Site/webapp/wdkCustomization/js/client/components/homepage/SearchPane.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/SearchPane.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useState, useMemo } from 'react';
 
-import { memoize, noop } from 'lodash';
+import { memoize, noop, orderBy } from 'lodash';
 
 import { CategoriesCheckboxTree, Link, Tooltip, Icon, Loading, IconAlt } from 'wdk-client/Components';
 import { LinksPosition } from 'wdk-client/Components/CheckboxTree/CheckboxTree';
-import { CategoryTreeNode, getDisplayName, getTargetType, getRecordClassUrlSegment, getTooltipContent } from 'wdk-client/Utils/CategoryUtils';
+import { CategoryTreeNode, getDisplayName, getTargetType, getRecordClassUrlSegment, getTooltipContent, getLabel } from 'wdk-client/Utils/CategoryUtils';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
 import { useSessionBackedState } from 'wdk-client/Hooks/SessionBackedState';
 
@@ -12,6 +12,7 @@ import { combineClassNames } from './Utils';
 
 import './SearchPane.scss';
 import { decode, arrayOf, string } from 'wdk-client/Utils/Json';
+import { mapStructure } from 'wdk-client/Utils/TreeUtils';
 
 const cx = makeClassNameHelper('ebrc-SearchPane');
 const EXPANDED_BRANCHES_SESSION_KEY = 'homepage-left-panel-expanded-branch-ids';
@@ -36,17 +37,19 @@ export const SearchPane = (props: Props) => {
     setAreControlsExpanded(!areControlsExpanded);
   }, [ areControlsExpanded ]);
 
+  const alphabetizedSearchTree = useAlphabetizedSearchTree(props.searchTree);
+
   return (
     <nav className={combineClassNames(cx(), props.containerClassName)}>
       <h2>
-        Search for...
+        Search the Data
         <button type="button" className="link" onClick={toggleAreControlsExpanded}>
           <IconAlt fa="wrench" className="fa-flip-horizontal" />
         </button>
       </h2> 
       <div className={cx('CheckboxTreeContainer', areControlsExpanded ? 'controls_expanded' : 'controls_collapsed')}>
         <SearchCheckboxTree 
-          searchTree={props.searchTree} 
+          searchTree={alphabetizedSearchTree} 
           searchTerm={searchTerm}
           expandedBranches={expandedBranches}
           setSearchTerm={setSearchTerm}
@@ -55,6 +58,32 @@ export const SearchPane = (props: Props) => {
       </div>
     </nav>
   );
+}
+
+function useAlphabetizedSearchTree(searchTree?: CategoryTreeNode) {
+  const result = useMemo(() => {
+    if (searchTree == null) {
+      return undefined;
+    }
+
+    return mapStructure(
+      (node, mappedChildren: CategoryTreeNode[]) => {
+        return {
+          ...node,
+          children: getLabel(node)?.endsWith('RecordClass')
+            ? mappedChildren
+            : orderBy(
+              [...mappedChildren],
+              getDisplayName
+            )
+        };
+      },
+      node => node.children,
+      searchTree
+    );
+  }, [ searchTree ]);
+
+  return result;
 }
 
 type SearchCheckboxTreeProps = {
@@ -92,17 +121,17 @@ export const SearchCheckboxTree = (props: SearchCheckboxTreeProps) => {
       />;
 }
 
-const renderNode = (node: any) => {
+const renderNode = (node: any, path: number[] | undefined) => {
   const displayName = getDisplayName(node);
   const displayElement = getTargetType(node) === 'search'
     ? <Link to={`/search/${getRecordClassUrlSegment(node)}/${node.wdkReference.urlSegment}`}>
         <IconAlt fa="search" />
         {displayName}
       </Link>
-    : <span>{displayName}</span>
+    : <span className={path?.length === 2 ? 'SubcategoryText' : undefined}>{displayName}</span>
 
   const tooltipContent = getTooltipContent(node);
-  
+
   return tooltipContent
     ? (
       <Tooltip content={tooltipContent}>
