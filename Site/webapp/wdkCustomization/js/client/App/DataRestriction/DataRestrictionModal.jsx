aaf90c9b7ad4bcab2023080313905d46e5950ec6
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-02-04 16:32:02 -0500

    new study access restriction level popups

--- Site/webapp/wdkCustomization/js/client/App/DataRestriction/DataRestrictionModal.jsx
+++ Site/webapp/wdkCustomization/js/client/App/DataRestriction/DataRestrictionModal.jsx
@@ -11,47 +11,80 @@ import './DataRestrictionModal.scss';
 class DataRestrictionModal extends React.Component {
   constructor (props) {
     super(props);
-    this.renderButtons = this.renderButtons.bind(this);
-    this.renderPolicyNotice = this.renderPolicyNotice.bind(this);
     this.renderRestrictionMessage = this.renderRestrictionMessage.bind(this);
+    this.renderPolicyNotice = this.renderPolicyNotice.bind(this);
+    this.renderButtons = this.renderButtons.bind(this);
+  }
+
+  renderRestrictionMessage () {
+    const { study, action, webAppUrl } = this.props;
+    const message = getRestrictionMessage({ study, action });
+    const studyPageUrl = webAppUrl + '/app' + study.route;
+    return (study.access === 'noaccessreq') 
+      ? (
+        <div>
+          <h2>The {safeHtml(study.name)} study is not yet publicly available.</h2>
+          <hr />
+          <p>Please see the <a href={studyPageUrl}>{safeHtml(study.name)} study page</a> to learn more about the study and how to request access to the data.</p>
+        </div>
+        ) 
+      : (
+        <div>
+          <h2>The {safeHtml(study.name)} study has data access restrictions.</h2>
+          <hr />
+          <p>{message}</p>
+        </div>
+        );
   }
 
   renderPolicyNotice () {
     const { study, webAppUrl } = this.props;
     const policyUrl = getPolicyUrl(study, webAppUrl);
-    return !policyUrl ? null : (
-      <p>
-        The data from this study requires approval to download and use in research projects.
-        Please read the <a href={policyUrl} target="_blank">{safeHtml(study.name)} Data Access and Use Policy.</a>
-      </p>
-    );
-  }
-
-  renderRestrictionMessage () {
-    const { study, action } = this.props;
-    const message = getRestrictionMessage({ study, action });
-    return (
-      <div>
-        <h2>The {safeHtml(study.name)} study has data access restrictions.</h2>
-        <hr />
-        <p>{message}</p>
-      </div>
+    return (study.access === 'noaccessreq')
+      ? null
+      : !policyUrl
+        ? null
+        : (
+          <p>
+            The data from this study requires approval to download and use in research projects.
+            Please read the <a href={policyUrl} target="_blank">{safeHtml(study.name)} Data Access and Use Policy.</a>
+          </p>
     );
   }
 
   renderButtons () {
     const { action, study, user, showLoginForm, onClose, webAppUrl } = this.props;
-
     const strict = isActionStrict(action);
     const approvalRequired = actionRequiresApproval({ action, study });
-    return (
-      <div className="DataRestrictionModal-Buttons">
-        {!user.isGuest ? null : (
-          <button onClick={() => showLoginForm(window.location.href)} className="btn">
-            Log In
-            <Icon fa="sign-in right-side" />
-          </button>
-        )}
+    return (study.access === 'noaccessreq') 
+      ? (
+        <div className="DataRestrictionModal-Buttons">
+          {!strict
+          ? (
+            <button className="btn" onClick={onClose}>
+              Dismiss
+              <Icon fa="times right-side" />
+            </button>
+          )
+          : (
+            <a href="/" title="Go Home">
+              <button className="btn">
+                Return to Home Page
+                <Icon fa="home right-side" />
+              </button>
+            </a>
+            )
+          }
+        </div>
+        ) 
+      : (
+        <div className="DataRestrictionModal-Buttons">
+          {!user.isGuest ? null : (
+            <button onClick={() => showLoginForm(window.location.href)} className="btn">
+              Log In
+              <Icon fa="sign-in right-side" />
+            </button>
+          )}
         {!approvalRequired ? null : (
           <button onClick={() => {
             const loggedInUrl = `${webAppUrl}/app/request-access/${study.id}?redirectUrl=${encodeURIComponent(window.location.href)}`;
