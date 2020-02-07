5657b27f8ebb00b26d1394f6c59dc0a420947b88
Dave Falke <dmfalke@gmail.com>
2020-02-07 14:44:25 -0500

    add arrow with link to previous search results

--- Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
+++ Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import { useSessionBackedState } from 'wdk-client/Hooks/SessionBackedState';
 import { useWdkService } from 'wdk-client/Hooks/WdkServiceHook';
 import { makeClassNameHelper } from "wdk-client/Utils/ComponentUtils";
 import { DatasetParam } from 'wdk-client/Utils/WdkModel';
@@ -7,6 +8,7 @@ import { SITE_SEARCH_ROUTE, SEARCH_TERM_PARAM } from './SiteSearchConstants';
 import { useQueryParams } from 'ebrc-client/hooks/queryParams';
 
 import './SiteSearch.scss';
+import { Tooltip } from 'wdk-client/Components';
 
 const cx = makeClassNameHelper("SiteSearch");
 
@@ -39,8 +41,21 @@ export function SiteSearchInput() {
     if (input instanceof HTMLInputElement) onSearch(input.value);
   }, [ onSearch ]);
   
+  const [ lastSearchQueryString, setLastSearchQueryString] = useSessionBackedState<string>(
+    '',
+    'ebrc/site-search/last-query-string',
+    (value: string) => value,
+    (value: string) => value
+  );
+
+  useEffect(() => {
+    if (location.pathname === SITE_SEARCH_ROUTE) {
+      setLastSearchQueryString(location.search.slice(1));
+    }
+  }, [ location ]);
+
   return (
-    <form onSubmit={handleSubmit} className={cx("--SearchBox")}>
+    <form action={SITE_SEARCH_ROUTE} onSubmit={handleSubmit} className={cx("--SearchBox")}>
       <input
         type="input"
         name={SEARCH_TERM_PARAM}
@@ -48,6 +63,13 @@ export function SiteSearchInput() {
         defaultValue={searchString}
         placeholder={placeholderText}
       />
+      {location.pathname !== SITE_SEARCH_ROUTE && lastSearchQueryString && (
+        <Tooltip content="Go to your last search result view">
+          <button type="button" onClick={() => history.push(`${SITE_SEARCH_ROUTE}?${lastSearchQueryString}`)}>
+            <i className="fa fa-long-arrow-left"/>
+          </button>
+        </Tooltip>
+      )}
       <button type="submit">
         <i className="fa fa-search" />
       </button>
