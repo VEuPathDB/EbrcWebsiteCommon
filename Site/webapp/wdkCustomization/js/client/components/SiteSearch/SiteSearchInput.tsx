250fcce40f42e3576df597bd43f842cd1b2ad009
Dave Falke <dmfalke@gmail.com>
2020-02-07 13:32:04 -0500

    Remove retention of organism, update placeholder

--- Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
+++ Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
@@ -1,66 +1,36 @@
-import { castArray, memoize, partial } from 'lodash';
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import { useSessionBackedState } from 'wdk-client/Hooks/SessionBackedState';
 import { useWdkService } from 'wdk-client/Hooks/WdkServiceHook';
 import { makeClassNameHelper } from "wdk-client/Utils/ComponentUtils";
-import { decodeOrElse, arrayOf, string } from 'wdk-client/Utils/Json';
 import { DatasetParam } from 'wdk-client/Utils/WdkModel';
-import { SITE_SEARCH_ROUTE, SEARCH_TERM_PARAM, ORGANISM_PARAM } from './SiteSearchConstants';
+import { SITE_SEARCH_ROUTE, SEARCH_TERM_PARAM } from './SiteSearchConstants';
 import { useQueryParams } from 'ebrc-client/hooks/queryParams';
 
 import './SiteSearch.scss';
 
 const cx = makeClassNameHelper("SiteSearch");
 
-const parseRawOrganism: (raw: string) => string[] = memoize(partial(decodeOrElse, arrayOf(string), []));
-
 export function SiteSearchInput() {
   const location = useLocation();
   const history = useHistory();
-  const [ storedOrganisms, updateStoredOrganisms ] = useSessionBackedState(
-    [],
-    'site-search/organisms',
-    JSON.stringify,
-    parseRawOrganism
-  );
-  
-  const [ params ] = useQueryParams(SEARCH_TERM_PARAM, ORGANISM_PARAM);
+  const [ params ] = useQueryParams(SEARCH_TERM_PARAM);
 
   const onSearch = useCallback((searchString: string) => {
-    const organismQueryPart = storedOrganisms.map(org => `${ORGANISM_PARAM}=${encodeURIComponent(org)}`).join('&');
-    history.push(`${SITE_SEARCH_ROUTE}?${SEARCH_TERM_PARAM}=${encodeURIComponent(searchString)}` +
-      (organismQueryPart ? `&${organismQueryPart}` : ''));
-  }, [ history, storedOrganisms ]);
-
-  useEffect(() => {
-    if (location.pathname !== SITE_SEARCH_ROUTE) return;
-    updateStoredOrganisms(castArray(params.organisms || []));
-  }, [ params.organisms, location.pathname ])
+    history.push(`${SITE_SEARCH_ROUTE}?${SEARCH_TERM_PARAM}=${encodeURIComponent(searchString)}`);
+  }, [ history ]);
 
   const searchString = location.pathname !== SITE_SEARCH_ROUTE ? ''
     : Array.isArray(params.q) ? params.q[0] : params.q;
 
   const placeholderText = useWdkService(async wdkService => {
-    let eg = '';
-
-    try {
-      const [ idSearch, textSearch ] = await Promise.all([
-        wdkService.getQuestionAndParameters('GeneByLocusTag').catch(),
-        wdkService.getQuestionAndParameters('GenesByTextSearch').catch()
-      ]);
-      
-      const id = idSearch?.parameters.find((p): p is DatasetParam => p.name === 'ds_gene_ids')?.defaultIdList;
-      const text = textSearch?.parameters.find(p => p.name === 'text_expression')?.initialDisplayValue;
-      const egValues = [ id, text ].filter(v => v).join(' or ');
-      eg = egValues ? `... e.g., ${egValues}` : '';
-    }
-    catch {
-      // ignore error
-    }
-    
-    const { displayName } = await wdkService.getConfig();
-    return `Search ${displayName}${eg}`;
+    const [ idSearch, textSearch ] = await Promise.all([
+      wdkService.getQuestionAndParameters('GeneByLocusTag').catch(),
+      wdkService.getQuestionAndParameters('GenesByTextSearch').catch()
+    ]);
+    const id = idSearch?.parameters.find((p): p is DatasetParam => p.name === 'ds_gene_ids')?.defaultIdList;
+    const text = textSearch?.parameters.find(p => p.name === 'text_expression')?.initialDisplayValue;
+    const examples = [ `*`, id, text, `"oxo group"` ].filter(v => v).join(' or ');
+    return 'E.g.,' + examples;
   })
 
   const inputRef = useRef<HTMLInputElement>(null);
