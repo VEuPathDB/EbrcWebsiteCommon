63c643fa59fc5fa233fe6f9f0904d2aeecdc1e6e
Jamie Long <jtlong3rd@gmail.com>
2020-03-06 15:06:22 -0500

    Allow customizable verbiage for the description of the region

--- Site/webapp/wdkCustomization/js/client/components/reporters/FastaGenomicSequenceReporterForm.jsx
+++ Site/webapp/wdkCustomization/js/client/components/reporters/FastaGenomicSequenceReporterForm.jsx
@@ -6,62 +6,67 @@ import SrtHelp from '../SrtHelp';
 
 let util = Object.assign({}, ComponentUtils, ReporterUtils);
 
-let FastaGenomicSequenceReporterForm = props => {
+export let fastaGenomicSequenceReporterFormFactory = regionDescription => {
+  const FastaGenomicSequenceReporterForm = props => {
+    let { formState, updateFormState, onSubmit, includeSubmit } = props;
+    let getUpdateHandler = fieldName => util.getChangeHandler(fieldName, updateFormState, formState);
 
-  let { formState, updateFormState, onSubmit, includeSubmit } = props;
-  let getUpdateHandler = fieldName => util.getChangeHandler(fieldName, updateFormState, formState);
-
-  return (
-    <div>
-      <h3>Choose the region of the sequence(s):</h3>
-      <div style={{margin:"2em"}}>
-        <Checkbox name="revComp" value={formState.revComp} onChange={getUpdateHandler('revComp')}/> Reverse & Complement
-      </div>
-      <div style={{margin:"2em"}}>
-        <b>Nucleotide positions:</b>
-        <TextBox name="start" value={formState.start} onChange={getUpdateHandler('start')} size="6"/> to
-        <TextBox name="end" value={formState.end} onChange={getUpdateHandler('end')} size="6"/> (0 = end)
-      </div>
-      <hr/>
-      <h3>Download Type:</h3>
-      <div style={{marginLeft:"2em"}}>
-        <RadioList name="attachmentType" value={formState.attachmentType} items={util.attachmentTypes}
-            onChange={getUpdateHandler('attachmentType')}/>
-      </div>
-      { includeSubmit &&
-        <div style={{margin:'0.8em'}}>
-          <button className="btn" type="submit" onClick={onSubmit}>Get Sequences</button>
-        </div>
-      }
+    return (
       <div>
+        <h3>Choose the {regionDescription} of the sequence(s):</h3>
+        <div style={{margin:"2em"}}>
+          <Checkbox name="revComp" value={formState.revComp} onChange={getUpdateHandler('revComp')}/> Reverse & Complement
+        </div>
+        <div style={{margin:"2em"}}>
+          <b>Nucleotide positions:</b>
+          <TextBox name="start" value={formState.start} onChange={getUpdateHandler('start')} size="6"/> to
+          <TextBox name="end" value={formState.end} onChange={getUpdateHandler('end')} size="6"/> (0 = end)
+        </div>
         <hr/>
-        <h3>Options:</h3>
-        <ul>
-          <li>
-            <i><b>complete sequence</b></i> to retrieve the complete sequence
-            for the requested genomic regions, use "Nucleotide positions 1 to 10000"
-          </li>
-          <li>
-            <i><b>specific sequence region</b></i> to retrieve a specific region
-            for the requested genomic regions, use "Nucleotide positions "
-            <i>x</i> to <i>y</i>, where <i>y</i> is greater than <i>x</i>
-          </li>
-        </ul>
-        <hr/>
+        <h3>Download Type:</h3>
+        <div style={{marginLeft:"2em"}}>
+          <RadioList name="attachmentType" value={formState.attachmentType} items={util.attachmentTypes}
+              onChange={getUpdateHandler('attachmentType')}/>
+        </div>
+        { includeSubmit &&
+          <div style={{margin:'0.8em'}}>
+            <button className="btn" type="submit" onClick={onSubmit}>Get Sequences</button>
+          </div>
+        }
+        <div>
+          <hr/>
+          <h3>Options:</h3>
+          <ul>
+            <li>
+              <i><b>complete sequence</b></i> to retrieve the complete sequence
+              for the requested genomic regions, use "Nucleotide positions 1 to 10000"
+            </li>
+            <li>
+              <i><b>specific sequence region</b></i> to retrieve a specific region
+              for the requested genomic regions, use "Nucleotide positions
+              <i>x</i> to <i>y</i>", where <i>y</i> is greater than <i>x</i>
+            </li>
+          </ul>
+          <hr/>
+        </div>
+        <SrtHelp/>
       </div>
-      <SrtHelp/>
-    </div>
-  );
+    );
+  };
+
+  FastaGenomicSequenceReporterForm.getInitialState = () => ({
+    formState: {
+      attachmentType: 'plain',
+      revComp: true,
+      start: 1,
+      end: 0
+    },
+    formUiState: {}
+  });
+
+  return FastaGenomicSequenceReporterForm;
 };
 
-FastaGenomicSequenceReporterForm.getInitialState = () => ({
-  formState: {
-    attachmentType: 'plain',
-    revComp: true,
-    start: 1,
-    end: 0
-  },
-  formUiState: {}
-});
 
-export default FastaGenomicSequenceReporterForm;
+
+export default fastaGenomicSequenceReporterFormFactory('region');
