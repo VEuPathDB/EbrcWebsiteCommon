b95039fa1fb30a5decb5f473b872a68dc6132f9e
Jamie Long <jtlong3rd@gmail.com>
2020-03-18 11:02:39 -0400

    Update footer for ClinEpi/Microbiome

--- Site/webapp/wdkCustomization/js/client/components/Footer.jsx
+++ Site/webapp/wdkCustomization/js/client/components/Footer.jsx
@@ -3,20 +3,28 @@ import { connect } from 'react-redux';
 import NewWindowLink from './NewWindowLink';
 import { formatReleaseDate } from '../util/formatters';
 
+import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
+
+import 'ebrc-client/components/homepage/ProjectLink.scss';
+
+const projectLinkCx = makeClassNameHelper('ebrc-ProjectLink');
+
 const projects = [
-  [ 'https', 'VEuPathDB' ],
-  [ 'http', 'AmoebaDB' ],
-  [ 'http', 'CryptoDB' ],
-  [ 'http', 'FungiDB' ],
-  [ 'http', 'GiardiaDB' ],
-  [ 'http', 'MicrosporidiaDB' ],
-  [ 'http', 'PiroplasmaDB' ],
-  [ 'http', 'PlasmoDB' ],
-  [ 'http', 'ToxoDB' ],
-  [ 'http', 'TrichDB' ],
-  [ 'http', 'TriTrypDB' ],
-  [ 'http', 'VectorBase' ],
-  [ 'http', 'OrthoMCL' ],
+  'VEuPathDB',
+  'AmoebaDB',
+  'CryptoDB',
+  'FungiDB',
+  'GiardiaDB',
+  'MicrosporidiaDB',
+  'PiroplasmaDB',
+  'PlasmoDB',
+  'ToxoDB',
+  'TrichDB',
+  'TriTrypDB',
+  'VectorBase',
+  'OrthoMCL',
+  'ClinEpiDB',
+  'MicrobiomeDB'
 ];
 
 const enhance = connect(state => state.globalData, null)
@@ -42,12 +50,18 @@ export default enhance(function Footer(props) {
 
       <div>
         <ul className="site-icons">
-          {projects.map(([ protocol, project ]) =>
-            <li title={`${project}.org`} key={project}>
-              <a href={`${protocol}://${project.toLowerCase()}.org`} className={project}>
-                {protocol}://{project.toLowerCase()}.org
-            </a>
-            </li>
+          {projects.map(project =>
+            <React.Fragment key={project}>
+              <li title={`${project}.org`} className={projectLinkCx()}>
+                <a href={`https://${project.toLowerCase()}.org`} className={project}>
+                  https://{project.toLowerCase()}.org
+                </a>
+              </li>
+              {
+                project === 'VectorBase' &&
+                <li className="divider"></li>
+              }
+            </React.Fragment>
           )}
         </ul>
       </div>
