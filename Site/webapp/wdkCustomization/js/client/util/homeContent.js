dfa41d95da3caf7d9d31112b4c42180f28ef6d09
Jamie Long <jtlong3rd@gmail.com>
2020-03-31 16:16:06 -0400

    Move studyFilter creation into a utility

--- Site/webapp/wdkCustomization/js/client/util/homeContent.js
+++ Site/webapp/wdkCustomization/js/client/util/homeContent.js
@@ -1,4 +1,22 @@
+import React from 'react';
+
+import { Seq } from 'wdk-client/Utils/IterableUtils';
 import { parseSearchQueryString, areTermsInString } from 'wdk-client/Utils/SearchUtils';
+
+import { CategoryIcon } from 'ebrc-client/App/Categories';
+
+export function studyFilters(studies) {
+  return Seq.from(studies.entities || [])
+    .flatMap(study => study.categories)
+    .orderBy(c => c)
+    .uniq()
+    .map(category => ({
+      id: category,
+      display: <CategoryIcon category={category}/>,
+      predicate: study => study.categories.includes(category)
+    }))
+    .toArray();
+}
 
 export function studyMatchPredicate(searchString, filterString) {
   const terms = parseSearchQueryString(filterString);
