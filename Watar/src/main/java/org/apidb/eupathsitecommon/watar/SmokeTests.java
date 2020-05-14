b641d1e38e2ebfbfc53330b5211001ead40cbbf4
John Brestelli <jbrestell@gmail.com>
2020-05-14 16:49:28 -0400

    add selenium to pom and get all HtmlUnit Tests working

--- Watar/src/main/java/org/apidb/eupathsitecommon/watar/SmokeTests.java
+++ Watar/src/main/java/org/apidb/eupathsitecommon/watar/SmokeTests.java
@@ -10,18 +10,24 @@ import org.testng.annotations.Test;
 
 import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.UnexpectedPage;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.WebResponse;
-import com.gargoylesoftware.htmlunit.html.DomElement;
+//import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
 
 public class SmokeTests {
 
+    static final int HTTP_RESPONSE_OK = 200;
     static final WebClient browser;
+
     public String baseurl;
     public String webappname;
-        
+    public boolean isPortal;
+    
     static {
         browser = new WebClient();
         browser.getOptions().setThrowExceptionOnFailingStatusCode(false);
@@ -32,8 +38,9 @@ public class SmokeTests {
     }
 
     public SmokeTests() {
-        baseurl = System.getProperty("baseurl");
-        webappname = System.getProperty("webappname");
+      baseurl = System.getProperty("baseurl");
+      webappname = System.getProperty("webappname");
+      isPortal = baseurl.toLowerCase().contains("eupathdb");
     }
 
     /** the home page for EuPathDB portal may depend on functionality
@@ -51,7 +58,8 @@ public class SmokeTests {
           groups = { "webservice" })
     public void WsfServicePage_HttpHeaderStatusIsOK() throws Exception {
         String url = baseurl + "/" + webappname + Utilities.WSF_PATH;
-        assertHeaderStatusMessageIsOK(url);
+        assertJsonStatusMessageIsOK(url);
+        //assertHeaderStatusMessageIsOK(url);
     }
 
     @Test(description="Assert HTTP header status is 200 OK for News url as test of valid XML record.", 
@@ -75,7 +83,9 @@ public class SmokeTests {
     public void GeneRecordPage_HttpHeaderStatusIsOK(String geneId) throws Exception {
         if (geneId == null) throw new Exception("unable to get gene id for testing");
         String url = baseurl + "/" + webappname + Utilities.GENE_RECORD_PATH_TMPL + geneId;
-        assertHeaderStatusMessageIsOK(url);
+        if(!isPortal) {
+          assertHeaderStatusMessageIsOK(url);
+        }   
     }
 
     @Test(description="Assert valid JSON from /dashboard/json API.", 
@@ -103,26 +113,32 @@ public class SmokeTests {
     }
 
     /** 
-      * Returns the default value from the Gene ID Quick Search form on the front page.
-    **/
-    @DataProvider(name="defaultGeneId")
-    private Object[][] getDefaultGeneIdFromQuickSearchForm() throws Exception {
-        try {
-        String url = baseurl + "/";
-        WebRequest request = new WebRequest(new URL(url), HttpMethod.GET);
-        HtmlPage page = (HtmlPage) browser.getPage(request);
-        for (DomElement element : page.getElementsByTagName("input")) {
-            if (element.getAttribute("name").contains("single_gene_id"))
-                return new Object[][] {{ element.getAttribute("value") }};
-        }
-        return null;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-        
-    }
+     * Returns the default value from the Gene ID Quick Search form on the front page.
+   **/
+   @DataProvider(name="defaultGeneId")
+   private Object[][] getDefaultGeneIdFromQuickSearchForm() throws Exception {
+     try {
+       String url = baseurl + "/" + webappname + Utilities.GENE_ID_SEARCH;
+       WebRequest request = new WebRequest(new URL(url), HttpMethod.GET);
+       HtmlPage page = (HtmlPage) browser.getPage(request);
 
+       for (HtmlForm form : page.getForms()) {
+         if (form.getNameAttribute().contentEquals("questionForm")) {
+           HtmlTextArea contentArea = form.getTextAreaByName("ds_gene_ids_data");
+           String defaultGene = contentArea.getText();
+             return new Object[][] {{ defaultGene }};
+         }
+       }
+       return null;
+       } catch (Exception e) {
+           e.printStackTrace();
+           throw e;
+       }
+   
+   }
+
+   
+   
     /** 
       * Assert HEAD request returns 200 OK for the given url.
     **/
@@ -132,11 +148,31 @@ public class SmokeTests {
             WebRequest request = new WebRequest(new URL(url), HttpMethod.HEAD);
             HtmlPage page = (HtmlPage) browser.getPage(request);
             WebResponse response = page.getWebResponse();
-            assertEquals(response.getStatusMessage(), "OK",  "Wrong HTTP Status for " + url + " .");
+            //assertEquals(response.getStatusMessage(), "OK",  "Wrong HTTP Status for " + url + " .");
+            assertEquals(response.getStatusCode(), HTTP_RESPONSE_OK,  "Wrong Status " + response.getStatusMessage() + " for " + url + " .");
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
    
     }
+
+    /** 
+     * Assert request returns 200 OK for the given json url
+   **/
+   private void assertJsonStatusMessageIsOK(String url) throws Exception {
+
+       try {
+           WebRequest request = new WebRequest(new URL(url), HttpMethod.HEAD);
+           request.setAdditionalHeader("Content-type", "application/json");
+           UnexpectedPage page = (UnexpectedPage) browser.getPage(request);
+           WebResponse response = page.getWebResponse();
+           assertEquals(response.getStatusCode(), HTTP_RESPONSE_OK,  "Wrong Status " + response.getStatusMessage() + " for " + url + " .");
+       } catch (Exception e) {
+           e.printStackTrace();
+           throw e;
+       }
+  
+   }
+
 }
--- Watar/src/main/java/org/apidb/eupathsitecommon/watar/Utilities.java
+++ Watar/src/main/java/org/apidb/eupathsitecommon/watar/Utilities.java
@@ -2,8 +2,9 @@ package org.apidb.eupathsitecommon.watar;
 
 public class Utilities {
 
-  public static final String GENE_RECORD_PATH_TMPL = "/showRecord.do?name=GeneRecordClasses.GeneRecordClass&source_id=";
-  public static final String WSF_PATH = "/services/WsfService";
+  public static final String GENE_RECORD_PATH_TMPL = "/app/record/gene/";
+  public static final String WSF_PATH = "/service";
   public static final String NEWS_PATH = "/showXmlDataContent.do?name=XmlQuestions.News";
-  public static final String METHODS_PATH = "/showXmlDataContent.do?name=XmlQuestions.Methods";
+  public static final String METHODS_PATH = "/wdkCustomization/jsp/questions/XmlQuestions.Methods.jsp";
+  public static final String GENE_ID_SEARCH = "/showQuestion.do?questionFullName=GeneQuestions.GeneByLocusTag";
 }
