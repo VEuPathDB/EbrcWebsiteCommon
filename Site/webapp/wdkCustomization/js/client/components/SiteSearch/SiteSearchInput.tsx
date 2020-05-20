81efbede6a5feebcc0eea797d545aa2a76d127ac
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-05-20 14:05:27 -0400

    site search suggestions and footer new BRC logo, per NIAID requests
    (cherry picked from commit f692898191986461d0ec2dd9806aed7219697e70)

--- Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
+++ Site/webapp/wdkCustomization/js/client/components/SiteSearch/SiteSearchInput.tsx
@@ -34,8 +34,8 @@ export function SiteSearchInput() {
     ]);
     const id = idSearch?.parameters.find((p): p is DatasetParam => p.name === 'ds_gene_ids')?.defaultIdList;
     const text = textSearch?.parameters.find(p => p.name === 'text_expression')?.initialDisplayValue;
-    const examples = [ `*`, id, text, `"oxo group"` ].filter(v => v).join(' or ');
-    return 'Site Search e.g.,' + examples;
+    const examples = [ id, text, `"binding protein"` ].filter(v => v).join(' or ');
+    return 'Site search, e.g. ' + examples;
   }, []);
 
   const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
