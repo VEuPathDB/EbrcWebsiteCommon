69c41289bfaed7c497980e825f5189d2e88f2ee9
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-05-21 20:18:47 -0400

    typo

--- Site/webapp/wdkCustomization/js/client/components/homepage/Footer.tsx
+++ Site/webapp/wdkCustomization/js/client/components/homepage/Footer.tsx
@@ -48,7 +48,7 @@ export const Footer: FunctionComponent<Props> = ({ children, containerClassName,
     <div className={cx('SiteFamilyRow')}>
       <div className={cx('Copyright')}>
          <div><a href="https://www.niaid.nih.gov/research/bioinformatics-resource-centers">
-                <img title="NAIAD Bioinformatics Resource Centers" src="/assets/images/BRC_Logo_transparent-01.png">
+                <img title="NIAID Bioinformatics Resource Centers" src="/assets/images/BRC_Logo_transparent-01.png">
                 </img>
          </a></div>
   {/*      <div className={cx('Brand')}>
