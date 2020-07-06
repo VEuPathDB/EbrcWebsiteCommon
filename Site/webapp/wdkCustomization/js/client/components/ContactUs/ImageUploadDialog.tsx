63bce1362d57b2ec51e9969f01bc8b39045a2d7d
Jamie Long <jtlong3rd@gmail.com>
2020-07-06 12:52:24 -0400

    Initial commit of croppable paste zone for ImageUploadDialog (note that currently the crop selection is ignored when clicking OK)

--- Site/webapp/wdkCustomization/js/client/components/ContactUs/ImageUploadDialog.tsx
+++ Site/webapp/wdkCustomization/js/client/components/ContactUs/ImageUploadDialog.tsx
@@ -1,7 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import ReactCrop from 'react-image-crop';
 
 import { Dialog } from 'wdk-client/Components';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
+
+import 'react-image-crop/lib/ReactCrop.scss';
+
+import './ImageUploadDialog.scss';
 
 const cx = makeClassNameHelper('ImageUploadDialog');
 
@@ -13,13 +18,41 @@ interface Props {
 export function ImageUploadDialog({ onSubmit, onClose }: Props) {
   const { imageFile, imageUrl } = useImageFromClipboard();
 
+  const initialCrop = { unit: '%', x: 25, y: 25, height: 50, width: 50, aspect: undefined } as const;
+  const [ crop, setCrop ] = useState<ReactCrop.Crop | undefined>(initialCrop);
+
+  const resetCrop = useCallback(() => {
+    setCrop(initialCrop);
+  }, []);
+
+  useEffect(() => {
+    resetCrop();
+  }, [ imageFile, resetCrop ]);
+
+  useEffect(() => {
+    window.addEventListener('resize', resetCrop);
+
+    return () => {
+      window.removeEventListener('resize', resetCrop);
+    };
+  }, [ resetCrop ]);
+
   const onClickOk = useCallback(() => {
     onSubmit(imageFile as File);
   }, [ onSubmit, imageFile ]);
 
   return (
     <Dialog open modal className={cx()} onClose={onClose}>
-      <img className={cx('--Image')} src={imageUrl} />
+      <div className={cx('--PasteZone')}>
+        {
+          imageUrl &&
+          <ReactCrop
+            src={imageUrl}
+            crop={crop}
+            onChange={setCrop}
+          />
+        }
+      </div>
       <div className={cx('--Footer')}>
         <button disabled={imageFile == null} onClick={onClickOk} type="button">OK</button>
         <button onClick={onClose} type="button">Cancel</button>
