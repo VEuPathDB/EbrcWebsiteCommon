72a1cb50cf60b6f99ce58159c7314e51087fc5e2
Jamie Long <jtlong3rd@gmail.com>
2020-07-06 20:06:03 -0400

    Apply user-selected crop when uploading screenshots

--- Site/webapp/wdkCustomization/js/client/components/ContactUs/ImageUploadDialog.tsx
+++ Site/webapp/wdkCustomization/js/client/components/ContactUs/ImageUploadDialog.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import ReactCrop from 'react-image-crop';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import ReactCrop, { Crop } from 'react-image-crop';
 
 import { Dialog } from 'wdk-client/Components';
 import { makeClassNameHelper } from 'wdk-client/Utils/ComponentUtils';
@@ -19,7 +19,9 @@ export function ImageUploadDialog({ onSubmit, onClose }: Props) {
   const { imageFile, imageUrl } = useImageFromClipboard();
 
   const initialCrop = { unit: '%', x: 25, y: 25, height: 50, width: 50, aspect: undefined } as const;
-  const [ crop, setCrop ] = useState<ReactCrop.Crop | undefined>(initialCrop);
+  const [ crop, setCrop ] = useState<Crop | undefined>(initialCrop);
+  const fullImageRef = useRef<HTMLImageElement>(null);
+  const previewImageRef = useRef<HTMLImageElement>();
 
   const resetCrop = useCallback(() => {
     setCrop(initialCrop);
@@ -37,9 +39,13 @@ export function ImageUploadDialog({ onSubmit, onClose }: Props) {
     };
   }, [ resetCrop ]);
 
+  const onPreviewImageLoaded = useCallback((previewImage: HTMLImageElement) => {
+    previewImageRef.current = previewImage;
+  }, [ previewImageRef.current ]);
+
   const onClickOk = useCallback(() => {
-    onSubmit(imageFile as File);
-  }, [ onSubmit, imageFile ]);
+    submitCroppedImg(fullImageRef.current, previewImageRef.current, crop, onSubmit, imageFile);
+  }, [ fullImageRef.current, previewImageRef.current, crop, onSubmit, imageFile ]);
 
   return (
     <Dialog open modal className={cx()} onClose={onClose}>
@@ -47,11 +53,13 @@ export function ImageUploadDialog({ onSubmit, onClose }: Props) {
         {
           imageUrl &&
           <ReactCrop
+            onImageLoaded={onPreviewImageLoaded}
             src={imageUrl}
             crop={crop}
             onChange={setCrop}
           />
         }
+        <img ref={fullImageRef} src={imageUrl} style={{ display: 'none' }} />
       </div>
       <div className={cx('--Footer')}>
         <button disabled={imageFile == null} onClick={onClickOk} type="button">OK</button>
@@ -65,31 +73,31 @@ function useImageFromClipboard() {
   const [ pastedImage, setPastedImage ] = useState<File | undefined>(undefined);
   const [ pastedImageUrl, setPastedImageUrl ] = useState<string | undefined>(undefined);
 
+  const handlePaste = useCallback((event: Event) => {
+    if (
+      event instanceof ClipboardEvent &&
+      event.clipboardData?.items != null
+    ) {
+      const pastedItems = [...event.clipboardData.items];
+      const pastedImageItem = pastedItems.find(({ type }) => type.startsWith('image'));
+
+      if (pastedImageItem != null) {
+        const pastedImageFile = pastedImageItem.getAsFile();
+
+        if (pastedImageFile != null) {
+          setPastedImage(pastedImageFile);
+          setPastedImageUrl(URL.createObjectURL(pastedImageFile));
+        }
+      }
+    }
+  }, []);
+
   useEffect(() => {
     window.addEventListener('paste', handlePaste, false);
 
     return () => {
       window.removeEventListener('paste', handlePaste, false);
     };
-
-    function handlePaste(event: Event) {
-      if (
-        event instanceof ClipboardEvent &&
-        event.clipboardData?.items != null
-      ) {
-        const pastedItems = [...event.clipboardData.items];
-        const pastedImageItem = pastedItems.find(({ type }) => type.startsWith('image'));
-
-        if (pastedImageItem != null) {
-          const pastedImageFile = pastedImageItem.getAsFile();
-
-          if (pastedImageFile != null) {
-            setPastedImage(pastedImageFile);
-            setPastedImageUrl(URL.createObjectURL(pastedImageFile));
-          }
-        }
-      }
-    }
   }, []);
 
   useEffect(() => () => {
@@ -102,4 +110,69 @@ function useImageFromClipboard() {
     imageFile: pastedImage,
     imageUrl: pastedImageUrl
   };
+}
+
+// Adaptation of https://github.com/DominicTobias/react-image-crop#what-about-showing-the-crop-on-the-client
+function submitCroppedImg(
+  fullImage: HTMLImageElement | null,
+  previewImage: HTMLImageElement | undefined,
+  crop: Crop | undefined,
+  onSubmit: (file: File) => void,
+  uncroppedFile: File | undefined
+) {
+  if (fullImage == null) {
+    throw new Error('Could not obtain the image containing the pasted screenshot.');
+  }
+
+  if (previewImage == null) {
+    throw new Error('Could not obtain preview image for the pasted screenshot.');
+  }
+
+  if (uncroppedFile == null) {
+    throw new Error('Could not retrieve the file with the pasted screenshot.');
+  }
+
+  if (
+    crop == null ||
+    crop.x == null ||
+    crop.y == null ||
+    crop.height == null ||
+    crop.width == null
+  ) {
+    onSubmit(uncroppedFile);
+    return;
+  }
+
+  const canvas = document.createElement('canvas');
+  const scaleX = fullImage.naturalWidth / previewImage.width;
+  const scaleY = fullImage.naturalHeight / previewImage.height;
+  canvas.width = crop.width * scaleX / window.devicePixelRatio;
+  canvas.height = crop.height * scaleY / window.devicePixelRatio;
+  const ctx = canvas.getContext('2d');
+
+  if (ctx == null) {
+    throw new Error('Could not initialize canvas for cropped screenshot.');
+  }
+
+  ctx.drawImage(
+    fullImage,
+    crop.x * scaleX,
+    crop.y * scaleY,
+    crop.width * scaleX,
+    crop.height * scaleY,
+    0,
+    0,
+    crop.width * scaleX / window.devicePixelRatio,
+    crop.height * scaleY / window.devicePixelRatio
+  );
+
+  canvas.toBlob(blob => {
+    if (blob == null) {
+      throw new Error('Could not write the cropped screenshot canvas to a file');
+    }
+
+    const croppedFile = new File([ blob ], uncroppedFile.name);
+
+    onSubmit(croppedFile);
+  }, uncroppedFile.type, 1);
 }
