3b24a3d5937583cbeece6950b935e06678358dd6
Jamie Long <jtlong3rd@gmail.com>
2020-07-17 09:26:07 -0400

    Move RadioParams custom question page from api -> ebrc; convert ebrc pluginConfig to TypeScript

---
+++ Site/webapp/wdkCustomization/js/client/pluginConfig.ts
@@ -0,0 +1,113 @@
+import {
+  HistogramAnalysisPlugin,
+  WordCloudAnalysisPlugin,
+  StepAnalysisViewPlugin,
+  ResultTableSummaryViewPlugin,
+  StepAnalysisDefaultForm,
+  StepAnalysisDefaultResult,
+  StepAnalysisEupathExternalResult,
+} from 'wdk-client/Plugins';
+import { ClientPluginRegistryEntry } from 'wdk-client/Utils/ClientPlugin';
+
+import { EbrcDefaultQuestionForm } from 'ebrc-client/components/questions/EbrcDefaultQuestionForm';
+import { RadioParams } from 'ebrc-client/plugins/RadioParams';
+import QuestionWizardPlugin from 'ebrc-client/plugins/QuestionWizardPlugin';
+
+const ebrcPluginConfig: ClientPluginRegistryEntry<any>[] = [
+  {
+    type: 'attributeAnalysis',
+    name: 'wordCloud',
+    component: WordCloudAnalysisPlugin
+  },
+  {
+    type: 'attributeAnalysis',
+    name: 'histogram',
+    component: HistogramAnalysisPlugin
+  },
+  {
+    type: 'stepAnalysisView',
+    name: 'defaultStepAnalysisView',
+    component: StepAnalysisViewPlugin
+  },
+  {
+    type: 'summaryView',
+    name: '_default',
+    component: ResultTableSummaryViewPlugin
+  },
+  {
+    type: 'questionController',
+    test: ({ question }) =>
+      question?.properties?.websiteProperties?.includes('useWizard') ?? false,
+    component: QuestionWizardPlugin
+  },
+  {
+    type: 'questionForm',
+    test: ({ question }) =>
+      question?.properties?.['radio-params'] != null,
+    component: RadioParams
+  },
+  {
+    type: 'questionForm',
+    component: EbrcDefaultQuestionForm
+  },
+  {
+    type: 'stepAnalysisForm',
+    component: StepAnalysisDefaultForm
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'otu_abundance',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'alpha_diversity',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'beta_diversity',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'differential_abundance',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'correlation_app',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'person-graph-analysis',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'light-trap-plots',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-cont-table',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-event-dist',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    name: 'clinepi-summaries',
+    component: StepAnalysisEupathExternalResult
+  },
+  {
+    type: 'stepAnalysisResult',
+    component: StepAnalysisDefaultResult
+  },
+];
+
+export default ebrcPluginConfig;
---
+++ Site/webapp/wdkCustomization/js/client/plugins/RadioParams.tsx
@@ -0,0 +1,179 @@
+import React, { useCallback, useMemo } from 'react';
+
+import { get } from 'lodash';
+
+import { HelpIcon, IconAlt } from 'wdk-client/Components';
+import { Parameter, ParameterGroup } from 'wdk-client/Utils/WdkModel';
+import { makeClassNameHelper, safeHtml } from 'wdk-client/Utils/ComponentUtils';
+import { Seq } from 'wdk-client/Utils/IterableUtils';
+import { Props, Group } from 'wdk-client/Views/Question/DefaultQuestionForm';
+import { isMultiPick, isEnumParam, toMultiValueString, toMultiValueArray } from 'wdk-client/Views/Question/Params/EnumParamUtils';
+
+import { EbrcDefaultQuestionForm } from 'ebrc-client/components/questions/EbrcDefaultQuestionForm';
+
+import './RadioParams.scss';
+
+const cx = makeClassNameHelper('wdk-QuestionForm');
+
+type RadioParameterListProps = {
+  parameters: string[];
+  parameterMap: Record<string, Parameter>;
+  parameterElements: Record<string, React.ReactNode>;
+  radioParamSet: Set<string>;
+  activeRadioParam: string;
+  updateActiveRadioParam: (activeRadioParam: string) => void;
+  paramDependenciesUpdating: Record<string, boolean>;
+}
+
+function RadioParameterList(props: RadioParameterListProps) {
+  const {
+    parameters,
+    parameterMap,
+    parameterElements,
+    radioParamSet,
+    activeRadioParam,
+    updateActiveRadioParam,
+    paramDependenciesUpdating
+  } = props;
+
+  return (
+    <div className={cx('ParameterList')}>
+      {Seq.from(parameters)
+        .map(paramName => parameterMap[paramName])
+        .map(parameter => (
+          <div
+            key={parameter.name}
+            className={
+              radioParamSet.has(parameter.name) && parameter.name !== activeRadioParam
+                ? 'wdk-InactiveRadioParam'
+                : undefined
+            }
+            onFocus={
+              radioParamSet.has(parameter.name)
+                ? () => {
+                    updateActiveRadioParam(parameter.name);
+                  }
+                : undefined
+            }
+          >
+            <div className={cx('ParameterHeading')}>
+              {
+                radioParamSet.has(parameter.name) && (
+                  <input
+                    type="radio"
+                    name="radio-param"
+                    className={cx('RadioParamElement')}
+                    checked={parameter.name === activeRadioParam}
+                    onChange={() => {
+                      updateActiveRadioParam(parameter.name);
+                    }}
+                  />
+                )
+              }
+              <h2>
+                <HelpIcon>{safeHtml(parameter.help)}</HelpIcon> {parameter.displayName}
+                {paramDependenciesUpdating[parameter.name] && <IconAlt fa="circle-o-notch" className="fa-spin fa-fw" />}
+              </h2>
+            </div>
+            <div className={cx('ParameterControl')}>
+              {parameterElements[parameter.name]}
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+const NONSENSE_VALUE = 'N/A';
+const NONSENSE_VALUE_REGEX = /^(nil|N\/A)$/;
+
+const NONSENSE_MULTIPICK_STRING = toMultiValueString([ NONSENSE_VALUE ]);
+
+export const RadioParams: React.FunctionComponent<Props> = props => {
+  const radioParams: string[] = get(
+    props.state.question.properties,
+    'radio-params',
+    []
+  );
+  const radioParamSet = useMemo(
+    () => new Set(radioParams),
+    [ radioParams ]
+  );
+
+  const initialRadioParam = radioParams.find(
+    radioParam => {
+      const paramValueString = (props.state.paramValues[radioParam] || '').trim();
+
+      if (isMultiPick(props.state.question.parametersByName[radioParam])) {
+        const paramValue = toMultiValueArray(paramValueString);
+
+        return paramValue.length > 0 && !NONSENSE_VALUE_REGEX.test(paramValue[0]);
+      } else {
+        return paramValueString.length > 0 && !NONSENSE_VALUE_REGEX.test(paramValueString);
+      }
+    }
+  ) || radioParams[0];
+
+  const [ activeRadioParam, updateActiveRadioParam ] = React.useState(initialRadioParam);
+
+  const renderParamGroup = useCallback((group: ParameterGroup, props: Props) => {
+      const {
+        state: { question, groupUIState, paramDependenciesUpdating },
+        eventHandlers: { setGroupVisibility },
+        parameterElements
+      } = props;
+
+      return (
+        <Group
+          key={group.name}
+          searchName={question.urlSegment}
+          group={group}
+          uiState={groupUIState}
+          onVisibilityChange={setGroupVisibility}
+        >
+          <RadioParameterList
+            parameterMap={question.parametersByName}
+            parameterElements={parameterElements}
+            parameters={group.parameters}
+            radioParamSet={radioParamSet}
+            activeRadioParam={activeRadioParam}
+            updateActiveRadioParam={updateActiveRadioParam}
+            paramDependenciesUpdating={paramDependenciesUpdating}
+          />
+        </Group>
+      );
+    },
+    [ radioParamSet, activeRadioParam ]
+  );
+
+  const onSubmit = useCallback((e: React.FormEvent) => {
+    e.preventDefault();
+
+    radioParams.forEach(radioParam => {
+      if (radioParam !== activeRadioParam) {
+        const parameter = props.state.question.parametersByName[radioParam];
+        const paramValue = isMultiPick(parameter)
+          ? NONSENSE_MULTIPICK_STRING
+          : NONSENSE_VALUE
+
+        props.eventHandlers.updateParamValue({
+          searchName: props.state.question.urlSegment,
+          parameter,
+          paramValues: props.state.paramValues,
+          paramValue
+        });
+      }
+    });
+
+    return true;
+  }, [ props.state, props.eventHandlers.updateParamValue, activeRadioParam ]);
+
+  return (
+    <EbrcDefaultQuestionForm
+      {...props}
+      renderParamGroup={renderParamGroup}
+      onSubmit={onSubmit}
+      validateForm={false}
+    />
+  );
+};
