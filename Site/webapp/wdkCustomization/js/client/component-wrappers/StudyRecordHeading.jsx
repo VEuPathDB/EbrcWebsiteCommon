93b05bdb8e7b6836c3cd8473cc899663a2308153
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-08-16 23:08:05 -0400

    fix jsx syntax

--- Site/webapp/wdkCustomization/js/client/component-wrappers/StudyRecordHeading.jsx
+++ Site/webapp/wdkCustomization/js/client/component-wrappers/StudyRecordHeading.jsx
@@ -32,7 +32,7 @@ function StudyRecordHeading({ showSearches = false, showDownload = false, entrie
       {isPrereleaseStudy(study.access) && (
         <div style={{backgroundColor:'lightblue',padding:'0.5em', fontSize:'1.8em',margin:'1.5em 0 0'}} className='record-page-banner'>
           This study has not yet been released. <span style={{fontSize:'80%'}}>
-            For more information, please email {props.record.attributes.contact} at <a href="mailto:{study.email}">{study.email}</a>.</span>
+            For more information, please email {props.record.attributes.contact} at <a href={"mailto:" + study.email}>{study.email}</a>.</span>
         </div>
       )}
       {showDownload && (!isPrereleaseStudy(study.access)) && (
