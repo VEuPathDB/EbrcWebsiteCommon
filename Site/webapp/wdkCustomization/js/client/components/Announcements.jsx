f6da68aa464f3b888df71701730ef59211d7674c
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-08-27 19:19:54 -0400

    legacy portal link to new veupath site

--- Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
+++ Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
@@ -78,8 +78,9 @@ const siteAnnouncements = [
       return (
         <div style={{fontWeight: 'bold'}} key="beta">
           You are currently using the legacy {props.projectId} site. 
-          We encourage you to visit the <a target="_blank" href={`https://${props.projectId.toLowerCase()}.${props.projectId === 'SchistoDB' ? 'net' : 'org'}`}>
-            {props.projectId !== 'EuPathDB' ? props.projectId : 'VEuPathDB'} beta site</a> to experience our latest features and access any data updates. 
+          We encourage you to visit the 
+            <a target="_blank" href={`https://${props.projectId.toLowerCase()}.${props.projectId === 'SchistoDB' ? 'net' : 'org'}.${props.projectId !== 'EuPathDB' ? props.projectId : 'VEuPathDB'}`}>
+              {props.projectId !== 'EuPathDB' ? props.projectId : 'VEuPathDB'} beta site</a> to experience our latest features and access any data updates. 
           The legacy sites will be phased out soon.
         </div>
       );
