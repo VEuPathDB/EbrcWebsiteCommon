2b1f84a7779a96622d815b4bf9112a4dd1342ced
Cristina Aurrecoechea <aurreco@luffa.gacrc.uga.edu>
2020-08-27 20:12:15 -0400

    beta italics

--- Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
+++ Site/webapp/wdkCustomization/js/client/components/Announcements.jsx
@@ -55,7 +55,7 @@ const siteAnnouncements = [
     renderDisplay: props => {
       if ( isGenomicHomePage(props.projectId, props.location) ) return (
         <div key="live-beta">
-          Welcome to {props.displayName} beta where you will find the newest versions of our interface, features, tools and data.  
+          Welcome to {props.displayName} <i>beta</i> where you will find the newest versions of our interface, features, tools and data.  
           While we transition to making this beta site permanent, <a target="_blank" href={`https://legacy.${props.projectId.toLowerCase()}.${props.projectId === 'SchistoDB' ? 'net' : 'org'}`}>
             legacy.{props.projectId.toLowerCase()}.org</a> is still available. 
           Here is a <a target="_blank" href="https://upenn.co1.qualtrics.com/jfe/form/SV_9N2RTXq7ljpZnDv">form for sending your feedback</a> on the beta sites.
