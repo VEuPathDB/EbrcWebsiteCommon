45d092f605c425f83a9d94dc7f77bc878c33360d
Dave Falke <dmfalke@gmail.com>
2020-09-18 15:52:15 -0400

    Add more category cases

--- Site/webapp/wdkCustomization/js/client/App/Categories/CategoryUtils.js
+++ Site/webapp/wdkCustomization/js/client/App/Categories/CategoryUtils.js
@@ -3,12 +3,19 @@ import React from 'react';
 export function getCategoryColor (category) {
   if (!category) return null;
   switch (category.toLowerCase()) {
+    // This contains a mix of disease variables from https://webprotege.stanford.edu/#projects/719dd1bd-ffbb-4c15-99cc-050a233977ee/edit/Classes?selection=Class(%3Chttp://purl.obolibrary.org/obo/DOID_4%3E),
+    // non-standard disease names, and sample types
     case 'enteric':
     case 'diarrhea':
+    case 'diarrheal disease':
       return '#6738ff';  //#8874a3'; more muted
+    case 'hiv':
+    case 'aids':
+      return '#cb2b39';
     case 'malarial':
     case 'malaria':
       return '#ff6d0d';
+    case 'tuberculosis':
     case 'respiratory':
     case 'respiratory condition':
       return '#00aedb';
@@ -16,6 +23,7 @@ export function getCategoryColor (category) {
       return '#7bc043';
     case 'mixed':
       return '#00aedb';
+
     case 'human':
       return '#6738ff';
     case 'insect':
