800c944e842aee1e8bba0534fa1cc59a963d0fc0
Jamie Long <jtlong3rd@gmail.com>
2020-10-12 17:33:26 -0400

    Create API for staff entities

--- Site/webapp/wdkCustomization/js/client/StudyAccess/api.ts
+++ Site/webapp/wdkCustomization/js/client/StudyAccess/api.ts
@@ -1,6 +1,20 @@
 import {
-  createFetchApiRequestHandler
+  ApiRequestHandler,
+  createFetchApiRequestHandler,
+  createJsonRequest,
+  standardTransformer
 } from 'ebrc-client/util/api';
+import {
+  NewStaffRequest,
+  StaffList,
+  StaffPatch,
+  newStaffResponse,
+  staffList
+} from 'ebrc-client/StudyAccess/Types';
+
+const STAFF_PATH = '/staff';
+const PROVIDERS_PATH = '/dataset-providers';
+const END_USERS_PATH = '/dataset-end-users';
 
 export function createStudyAccessRequestHandler(
   baseStudyAccessUrl: string,
@@ -19,4 +33,56 @@ export function createStudyAccessRequestHandler(
     },
     fetchApi
   });
+}
+
+export function fetchStaffList(handler: ApiRequestHandler): Promise<StaffList> {
+  return handler({
+    path: STAFF_PATH,
+    method: 'GET',
+    transformResponse: standardTransformer(staffList)
+  });
+}
+
+export function newStaffEntry(
+  handler: ApiRequestHandler,
+  requestBody: NewStaffRequest
+) {
+  const request = createJsonRequest({
+    path: STAFF_PATH,
+    method: 'POST',
+    body: requestBody,
+    transformResponse: standardTransformer(newStaffResponse)
+  });
+
+  return handler(request);
+}
+
+export function updateStaffEntry(
+  handler: ApiRequestHandler,
+  staffId: number,
+  requestBody: StaffPatch
+) {
+  const request = createJsonRequest({
+    path: `${STAFF_PATH}/${staffId}`,
+    method: 'PATCH',
+    body: requestBody,
+    transformResponse: noContent
+  });
+
+  return handler(request);
+}
+
+export function deleteStaffEntry(
+  handler: ApiRequestHandler,
+  staffId: number
+) {
+  return handler({
+    path: `${STAFF_PATH}/${staffId}`,
+    method: 'DELETE',
+    transformResponse: noContent
+  });
+}
+
+async function noContent(body: unknown) {
+  return null;
 }
