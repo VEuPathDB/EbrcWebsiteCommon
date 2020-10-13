819508bcdbd5027dc89962f986ba259f7300859b
Jamie Long <jtlong3rd@gmail.com>
2020-10-13 16:17:39 -0400

    Introduce additional UserTable props

--- Site/webapp/wdkCustomization/js/client/components/StudyAccess/UserTable.tsx
+++ Site/webapp/wdkCustomization/js/client/components/StudyAccess/UserTable.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-export interface Props {
+import { MesaColumn, MesaSortObject } from 'wdk-client/Core/CommonTypes';
+
+export interface Props<R, C extends UserTableColumnKey<R>> {
   title: React.ReactNode;
-  // rows
-  // columns
-  // actions
-  // selectedRows
+  rows: R[];
+  // columns: UserTableColumns<R, C>;
+  // columnOrder: readonly C[];
+  // isRowSelected: (row: R) => boolean;
   // filterTerm
   // sortColumn
 }
+
+export type UserTableColumnKey<R> = keyof R & string;
+
+export interface UserTableSortObject<R, K extends UserTableColumnKey<R>> extends MesaSortObject {
+  columnKey: K;
+  direction: 'asc' | 'desc';
+};
+
+export interface UserTableColumn<R, K extends UserTableColumnKey<R>> extends MesaColumn<K> {
+  renderCell?: (props: { row: R, value: R[K] }) => React.ReactNode;
+  makeSearchableString?: (value: R[K]) => string;
+  makeOrder?: (row: R) => boolean | number | string;
+}
+
+export type UserTableColumns<R, C extends UserTableColumnKey<R>> = {
+  [K in C]: UserTableColumn<R, K>
+};
