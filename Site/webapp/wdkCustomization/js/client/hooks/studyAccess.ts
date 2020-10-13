5d1943a1d0869ea864c69baee8d3ec36bc0c5e13
Jamie Long <jtlong3rd@gmail.com>
2020-10-13 15:56:41 -0400

    Initial commit for user table config hooks

--- Site/webapp/wdkCustomization/js/client/hooks/studyAccess.ts
+++ Site/webapp/wdkCustomization/js/client/hooks/studyAccess.ts
@@ -1,11 +1,19 @@
 import { useMemo } from 'react';
 
-import { createStudyAccessRequestHandler } from 'ebrc-client/StudyAccess/api';
+import { usePromise } from 'wdk-client/Hooks/PromiseHook';
+
+import {
+  createStudyAccessRequestHandler,
+  fetchEndUserList,
+  fetchProviderList,
+  fetchStaffList
+} from 'ebrc-client/StudyAccess/api';
 import {
   DatasetProviderList,
   EndUserList,
   StaffList
 } from 'ebrc-client/StudyAccess/Types';
+import { ApiRequestHandler } from 'ebrc-client/util/api';
 
 export function useStudyAccessRequestHandler(
   baseStudyAccessUrl: string,
@@ -15,6 +23,57 @@ export function useStudyAccessRequestHandler(
     () => createStudyAccessRequestHandler(baseStudyAccessUrl, fetchApi),
     []
   );
+}
+
+export function useStaffTableConfig(handler: ApiRequestHandler) {
+  // FIXME: Fetch this data iff the user is a staff member
+  const { value } = usePromise(
+    async () => fetchStaffList(handler),
+    []
+  );
+
+  const staffTableRows = useMemo(
+    () => value && makeStaffTableRows(value),
+    [ value ]
+  );
+
+  return staffTableRows && {
+    rows: staffTableRows
+  };
+}
+
+export function useProviderTableConfig(handler: ApiRequestHandler, activeDatasetId: string) {
+  // FIXME: Fetch this data iff the user is a staff member or provider for the dataset
+  const { value } = usePromise(
+    async () => fetchProviderList(handler),
+    []
+  );
+
+  const providerTableRows = useMemo(
+    () => value && makeProviderTableRows(value, activeDatasetId),
+    [ activeDatasetId ]
+  );
+
+  return providerTableRows && {
+    rows: providerTableRows
+  };
+}
+
+export function useEndUserTableConfig(handler: ApiRequestHandler, activeDatasetId: string) {
+  // FIXME: Fetch this data iff the user is a staff member or provider for the dataset
+  const { value } = usePromise(
+    async () => fetchEndUserList(handler),
+    []
+  );
+
+  const endUserTableRows = useMemo(
+    () => value && makeEndUserTableRows(value, activeDatasetId),
+    [ activeDatasetId ]
+  );
+
+  return endUserTableRows && {
+    rows: endUserTableRows
+  };
 }
 
 function makeStaffTableRows(response: StaffList) {
