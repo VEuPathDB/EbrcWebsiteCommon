81b800bab40b745a9db2e16b77c9fb15351e3b28
Jamie Long <jtlong3rd@gmail.com>
2021-07-27 16:39:39 -0400

    Cleanup: rename "RestrictedComponent" to the more informative "RestrictedPage", and add some documentation clarifying its intended use

--- Client/src/App/DataRestriction/RestrictedPage.tsx
+++ Client/src/App/DataRestriction/RestrictedPage.tsx
@@ -9,13 +9,34 @@ interface Props {
   children: ReactNode;
 }
 
-export function RestrictedComponent({
+/**
+ * This component is used for rendering pages whose content (children)
+ * is protected by a Data Restriction.
+ * 
+ * The UX is as follows:
+ * 
+ * (1) While the approvalStatus is loading, the page contents are rendered inside a
+ *     "hidden" div
+ * 
+ * (2) Once the approvalStatus is done loading...
+ * 
+ * a. If the study does not exist (in the sense that it does not exist in the backend
+ *    OR the study is marked as "disabled" by the WDK model), render our usual "not found" page
+ *    instead of the page contents
+ * 
+ * b. If the user is approved to perform the action, display the page contents in an "unhidden" div
+ * 
+ * c. If the user is not approved to perform the action, display the page contents in a "blurred" div
+ *    which stops the propagation of any user-interaction-related DOM events
+ */
+export function RestrictedPage({
   approvalStatus,
   children,
 }: Props) {
   return approvalStatus === 'study-not-found' ? (
     <NotFoundController />
-  ) : // always wrap children with a div to prevent them from being unmounted
+  ) : // wrap children with a div to prevent them from being unmounted
+      // (to prevent loss of component state, premature cleanup of effects, etc)
   approvalStatus === 'loading' ? (
     <div style={{ visibility: 'hidden' }}>{children}</div>
   ) : approvalStatus === 'approved' ? (
