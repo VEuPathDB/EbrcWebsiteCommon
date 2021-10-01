76667bf655ed59b9de0faa174321ba3a27fa64f9
Dave Falke <dmfalke@gmail.com>
2021-09-30 22:33:05 -0400

    Filter eda-enabled study records using dataset record attribute

--- Client/src/wrapWdkService.js
+++ Client/src/wrapWdkService.js
@@ -1,4 +1,3 @@
-import { memoize } from 'lodash';
 import { useEda } from 'ebrc-client/config';
 import { ok } from '@veupathdb/wdk-client/lib/Utils/Json';
 
@@ -17,10 +16,8 @@ export default wdkService => ({
     });
 
     if (useEda) {
-      const studies = await getEdaStudies();
-      const datasetIds = new Set(studies.map(study => study.datasetId));
       // TODO Mark non-eda studies as prerelease, instead of removing
-      datasets.records = datasets.records.filter(record => datasetIds.has(record.attributes.dataset_id));
+      datasets.records = datasets.records.filter(record => record.attributes.eda_study_id != null);
     }
 
     return datasets;
@@ -30,10 +27,4 @@ export default wdkService => ({
     method: 'get',
     path: '/site-messages'
   })
-});
-
-const getEdaStudies = memoize(async () => {
-  const resp = await fetch('/eda-data/studies');
-  const { studies } = await resp.json();
-  return studies;
 });
