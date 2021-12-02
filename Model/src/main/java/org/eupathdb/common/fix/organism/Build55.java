e12b0fb5cf7810ea2babababd1b08e75a944edcb
Cristina Aurrecoechea <aurreco@durian.uga.apidb.org>
2021-12-02 18:28:50 -0500

    updates for org migration B55
    (cherry picked from commit c32415e7889af06a9ee977b7fb585f37ba413b3b)

---
+++ Model/src/main/java/org/eupathdb/common/fix/organism/Build55.java
@@ -0,0 +1,26 @@
+package org.eupathdb.common.fix.organism;
+
+import static java.util.Map.entry;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eupathdb.common.fix.organism.MigrateOrganisms.OrganismMigration;
+import org.gusdb.fgputil.ListBuilder;
+
+public class Build55 implements OrganismMigration {
+
+  @Override
+  public List<String> getProjectIds() {
+    return List.of("TriTrypDB","EuPathDB");
+  }
+
+  @Override
+  public Map<String, String> getOrganismMapping() {
+    return Map.ofEntries(
+      entry("Leishmania sp. MAR LEM2494","Leishmania martiniquensis LEM2494"),
+      entry("unclassified Leishmania",REMOVE_ENTRY)
+    );
+  }
+
+}
