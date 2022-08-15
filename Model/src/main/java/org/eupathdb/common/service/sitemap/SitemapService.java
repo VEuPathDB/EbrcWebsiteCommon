cc49306a17ad0223ba5d16b186f62b59e1e19f21
Ryan Doherty <tech@conical.org>
2022-08-15 11:51:11 -0400

    Comply with new WDK APIs

--- Model/src/main/java/org/eupathdb/common/service/sitemap/SitemapService.java
+++ Model/src/main/java/org/eupathdb/common/service/sitemap/SitemapService.java
@@ -1,87 +1,136 @@
 package org.eupathdb.common.service.sitemap;
 
-import static org.gusdb.fgputil.functional.Functions.fSwallow;
-
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
-import org.gusdb.fgputil.Tuples.ThreeTuple;
+import org.apache.log4j.Logger;
+import org.gusdb.fgputil.iterator.IteratorUtil;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkRuntimeException;
+import org.gusdb.wdk.model.answer.AnswerValue;
 import org.gusdb.wdk.model.answer.factory.AnswerValueFactory;
 import org.gusdb.wdk.model.answer.spec.AnswerSpec;
 import org.gusdb.wdk.model.question.Question;
-import org.gusdb.wdk.model.record.RecordClass;
+import org.gusdb.wdk.model.record.PrimaryKeyIterator;
 import org.gusdb.wdk.model.user.StepContainer;
 import org.gusdb.wdk.service.service.AbstractWdkService;
 
 @Path("sitemap")
 public class SitemapService extends AbstractWdkService {
 
+  private static final Logger LOG = Logger.getLogger(SitemapService.class);
+
   @GET
   @Produces(MediaType.TEXT_XML)
-  public String getSiteMap(@QueryParam("fmt") String fmt) {
+  public String getSiteMap(
+      @QueryParam("fmt") String fmt
+  ) {
     String urlBase = getContextUri();
-    String sitemaps = Arrays.stream(getWdkModel().getAllQuestionSets())
+    String sitemaps = Arrays
+        .stream(getWdkModel().getAllQuestionSets())
         .flatMap(questionSet -> Arrays.stream(questionSet.getQuestions()))
-        .filter(question -> question.getPropertyLists().containsKey("forSitemap")).map(question -> "<sitemap><loc>"
-            + urlBase + fmt.replace("{question}", question.getName()) + "</loc></sitemap>")
+        .filter(question -> question.getPropertyLists().containsKey("forSitemap"))
+        .map(question -> "<sitemap><loc>" + urlBase + fmt.replace("{question}", question.getName()) + "</loc></sitemap>")
         .collect(Collectors.joining(""));
 
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
-        "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" + sitemaps + "</sitemapindex>";
+    return
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
+        "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" +
+        sitemaps +
+        "</sitemapindex>";
   }
 
   @GET
   @Path("{questionName}")
   @Produces(MediaType.TEXT_XML)
-  public String getSiteMapForQuestion(@PathParam("questionName") String questionName, @QueryParam("fmt") String fmt) {
-    String urlBase = getContextUri();
+  public Response getSiteMapForQuestion(
+      @PathParam("questionName") String questionName,
+      @QueryParam("fmt") String fmt
+  ) throws WdkModelException {
 
     // find question for sitemap.xml
-    Optional<Question> maybeQuestion = getWdkModel().getQuestionByName(questionName)
-        .filter(question -> question.getPropertyLists().containsKey("forSitemap"));
+    Question question = getWdkModel().getQuestionByName(questionName)
+        .filter(q -> q.getPropertyLists().containsKey("forSitemap"))
+        .orElseThrow(() -> new NotFoundException());
 
-    Optional<RecordClass> maybeRecordClass = maybeQuestion
-        .flatMap(question -> getWdkModel().getRecordClassByFullName(question.getRecordClassName()));
+    // only zero-param questions are valid for site map
+    if (!question.getParamMap().isEmpty()) {
+      throw new WdkModelException("Invalid question '" + question.getFullName() + "' for site map: only questions with no params can be used.");
+    }
 
-    Optional<List<String[]>> maybeAllIds = maybeQuestion
-      .map(fSwallow(question ->
-        AnswerValueFactory.makeAnswer(
-          getSessionUser(),
-          AnswerSpec.builder(getWdkModel())
-            .setQuestionFullName(question.getFullName())
-            .buildRunnable(getSessionUser(), StepContainer.emptyContainer())
-        )
-        .getAllIds()
-      ));
+    // build answer for this question
+    AnswerValue answer = AnswerValueFactory.makeAnswer(
+        getSessionUser(),
+        AnswerSpec.builder(getWdkModel())
+          .setQuestionFullName(question.getFullName())
+          .buildRunnable(getSessionUser(), StepContainer.emptyContainer()));
 
-    Optional<ThreeTuple<Question, RecordClass, List<String[]>>> maybeData = maybeQuestion
-        .flatMap(question -> maybeRecordClass.flatMap(recordClass -> maybeAllIds
-            .map(allIds -> new ThreeTuple<Question, RecordClass, List<String[]>>(question, recordClass, allIds))));
+    return Response.ok(new RecordUrlDumper(getContextUri(), fmt, answer)).build();
+  }
 
-    return maybeData.map(data -> {
-      String urls = data.getThird().stream().map(id -> {
-        String idPart = Arrays.stream(id).map(part ->
-          URLEncoder.encode(part, StandardCharsets.UTF_8)).collect(Collectors.joining("/"));
+  private static class RecordUrlDumper implements StreamingOutput {
 
-        String url = urlBase + fmt
-          .replace("{recordClass}", URLEncoder.encode(data.getSecond().getUrlSegment(), StandardCharsets.UTF_8))
-          .replace("{id}", idPart);
+    private final String _urlBase;
+    private final String _urlFormat;
+    private final AnswerValue _answer;
 
-        return "<url><loc>" + url + "</loc></url>";
-      }).collect(Collectors.joining(""));
-      return "<?xml version='1.0' encoding='UTF-8'?>\n" +
-          "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" + urls + "</urlset>";
-    }).orElse("");
+    public RecordUrlDumper(String urlBase, String urlFormat, AnswerValue answer) {
+      _urlBase = urlBase;
+      _urlFormat = urlFormat;
+      _answer = answer;
+    }
+
+    @Override
+    public void write(OutputStream output) throws IOException, WebApplicationException {
+      // read IDs, convert to URLs and output XML
+      try (PrimaryKeyIterator allIds = _answer.getAllIds();
+           BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output))) {
+
+        writer.write("<?xml version='1.0' encoding='UTF-8'?>");
+        writer.newLine();
+        writer.write("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+        writer.newLine();
+
+        String recordClassUrlSegment = _answer.getAnswerSpec().getQuestion().getRecordClass().getUrlSegment();
+        String urlFormat = _urlFormat.replace("{recordClass}", URLEncoder.encode(recordClassUrlSegment, StandardCharsets.UTF_8));
+
+        for (String[] id : IteratorUtil.toIterable(allIds)) {
+
+          String idPart = Arrays.stream(id)
+              .map(part -> URLEncoder.encode(part, StandardCharsets.UTF_8))
+              .collect(Collectors.joining("/"));
+
+          writer.write("  <url><loc>");
+          writer.write(_urlBase);
+          writer.write(urlFormat.replace("{id}", idPart));
+          writer.write("</loc></url>");
+          writer.newLine();
+        }
+
+        writer.write("</urlset>");
+        writer.newLine();
+      }
+      catch (Exception e) {
+        LOG.error("Unable to stream stie map URL list", e);
+        throw new WdkRuntimeException("Unable to stream site map URL list", e);
+      }
+    }
   }
 }
