b4b1c2b3e41c4504a5e4337ab7b8da1d89f3021c
Ryan Doherty <rdoherty@upenn.edu>
2022-11-22 02:07:01 -0500

    Add service config to dashboard

--- Site/dashboard/view/services.php
+++ Site/dashboard/view/services.php
@@ -4,7 +4,9 @@
  * @package View
  */
 
-$stacks=system("bash -c \"sudo pconf | tail -n +15\"");
+$pconf=file_get_contents("/home/rdoherty/pconfLatest.json");
+$json=json_decode($pconf, true);
+$stacks=$json["stacks"];
 ?>
 <script>
   $( function() {
@@ -13,21 +15,69 @@ $stacks=system("bash -c \"sudo pconf | tail -n +15\"");
 </script>
 
 <h2>Microservices on this machine</h2>
-<h4>Note: this site may or may not be configured to point at these services
+<h3>Last Updated: <?php echo htmlspecialchars($json["lastUpdated"]); ?></h3>
+<hr/>
+<h3>Note: this site may or may not be configured to point at these services
 
 <div id="stackTabs">
   <ul>
     <?php
-      foreach ($stacks as $stackName) {
-        print("<li><a href=\"#$stackName\">$stackName</a></li>");
+      foreach ($stacks as $stack => $value) {
+        if ($stack != "empty") {
+          print("<li><a href=\"#$stack\">$stack</a></li>");
+        }
       }
     ?>
   </ul>
   <?php
-    foreach ($stacks as $stackName) {
-      print("<div id=\"$stackName\"><p>");
-      $stackContent=system("bash -c \"sudo pconf $stackName\"");
-      print("</p></div");
+    foreach ($stacks as $stack => $value) {
+      if ($stack != "empty") {
+        print("<div id=\"$stack\">");
+        print("<script>\n");
+        print("  \$( function() { \$( \"#$stack\" ).tabs(); } );\n");
+        print("</script>\n");
+        print("<b>Services deployed under compose stack: $stack</b>");
+        print("<ul>");
+        for ($i = 0; $i < count($value) - 1; $i+=2) {
+          $serviceName=explode(".", array_keys($value[$i])[0])[0];
+          print("<li><a href=\"#$stack$serviceName\">$serviceName</a></li>");
+        }
+        print("</ul>");
+        for ($i = 0; $i < count($value) - 1; $i+=2) {
+          $configKey=array_keys($value[$i])[0];
+          $serviceName=explode(".", $configKey)[0];
+          print("<div id=\"$stack$serviceName\">");
+          $config=$value[$i][$configKey];
+          $env=$config["Env"];
+          unset($config["Env"]);
+          $networkKey=array_keys($value[$i+1])[0];
+          $network=$value[$i+1][$networkKey];
+ 
+          $envJson=json_encode($env,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+          $configJson=json_encode($config,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+          $networkJson=json_encode($network,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+          // other ways to dump env besides pretty JSON
+          //$env=print_r($env,true);
+          //$dump=var_dump($env);
+
+          print("<h3>Environment</h3>");
+          print("<pre style=\"font-size:0.9em;font-weight:normal\">$envJson</pre>");
+
+          print("<hr/>");
+
+          print("<h3>Other Configuration</h3>");
+          print("<pre style=\"font-size:0.9em;font-weight:normal\">$configJson</pre>");
+
+          print("<hr/>");
+
+          print("<h3>Network Settings</h3>");
+          print("<pre style=\"font-size:0.9em;font-weight:normal\">$networkJson</pre>");
+
+          print("</div>");
+        }
+        print("</div>");
+      }
     }
   ?>
 </div>
