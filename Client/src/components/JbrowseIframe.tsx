023ee026a1bcb91af696edfef50a304795e1164e
Jeremy Myers <inernestmusic@gmail.com>
2023-03-06 15:13:30 -0500

    Replace zoom/scroll toggle with reset view button

--- Client/src/components/JbrowseIframe.tsx
+++ Client/src/components/JbrowseIframe.tsx
@@ -1,73 +1,104 @@
-import { Checkbox, HelpIcon } from '@veupathdb/wdk-client/lib/Components';
-import React, { SyntheticEvent, useCallback, useEffect, useRef, useState } from 'react';
+import React, { SyntheticEvent, useCallback, useState } from 'react';
 
 interface Props {
   jbrowseUrl: string;
   height: string;
 }
 
-interface JbrowseView {
-  behaviorManager: {
-    removeAll: () => void;
-    initialize: () => void;
-  }
-}
-
 declare global {
   interface Window {
     JBrowse: {
-      view: JbrowseView;
       afterMilestone: (
         milestoneName: string,
-        callback: (() => void)
+        callback: ((data: unknown) => void)
       ) => void;
+      subscribe: (
+        subscribeEvent: string,
+        callback: LocationCallback | HighlightCallback | TracksCallback
+      ) => void;
+      makeCurrentViewURL: () => string;
+      config: {
+        queryParams: {
+          loc: string,
+          highlight: string,
+          tracks: string,
+        };
+      }
     }
   }
 }
 
+interface JBrowseLocation {
+  ref: string,
+  start: number,
+  end: number,
+}
+
+type JBrowseHightlightSection = JBrowseLocation[];
+
+type LocationCallback = (data: JBrowseLocation) => void;
+type HighlightCallback = (data: JBrowseHightlightSection) => void;
+type TracksCallback = (data: string[][] | undefined) => void;
+
 export function JbrowseIframe(props: Props) {
   const { jbrowseUrl, height } = props;
-  const [ isLocked, setIsLocked ] = useState(true);
-  const onCheckboxToggle = useCallback(
-    newIsUnlockedValue => {
-      setIsLocked(!newIsUnlockedValue);
-    },
-    []
-  );
+  const [ key, setKey ] = useState<string>('jbrowse-iframe-' + Math.floor(Math.random()*1000000));
+  const [ isResetButtonDisabled, setIsResetButtonDisabled ] = useState<boolean>(true);
 
-  const jbrowseViewContainer = useRef<JbrowseView>();
-  const lockText = (
-    <small className="jbrowse-scroll-zoom-toggle-caption">
-      Scroll and zoom
-      {' '}
-      <HelpIcon>
-        <>
-          Select to enable using mouse / track pad for scrolling &amp; zoom
-          <br />
-          (double click to zoom in; shift-double click to zoom out)
-        </>
-      </HelpIcon>
-    </small>
-  );
-
-  useEffect(() => {
-    updateBehaviors();
-  }, [ isLocked ]);
+  const handleButtonState = useCallback(
+    (hasJBrowseViewChanged: boolean) => {
+      if (!hasJBrowseViewChanged) return;
+      setIsResetButtonDisabled(false);
+    }, []);
 
   function onLoad(event: SyntheticEvent<HTMLIFrameElement>) {
-    const JBrowse = event.currentTarget.contentWindow?.JBrowse
+    const JBrowse = event.currentTarget.contentWindow?.JBrowse;
     if (JBrowse == null) throw new Error("Could not load embedded JBrowse instance.");
     JBrowse.afterMilestone('completely initialized', function() {
-      jbrowseViewContainer.current = JBrowse.view;
-      updateBehaviors();
+      let areInitialTracksLoaded = false;
+      let isInitialLocationLoaded = false;
+      let initialStart: number | null = null;
+      const { highlight: initialHighlight, tracks: initialTracks } = JBrowse.config.queryParams;
+      JBrowse.subscribe(
+        '/jbrowse/v1/n/navigate',
+        function(data) {
+          if (!isInitialLocationLoaded) {
+            isInitialLocationLoaded = true;
+            initialStart = data.start;
+          }
+          if (initialStart == null) return
+          handleButtonState(initialStart !== data.start)
+        } as LocationCallback
+      );
+      JBrowse.subscribe(
+        '/jbrowse/v1/n/tracks/visibleChanged',
+        function(data) {
+          if (!data) return;
+          if (!areInitialTracksLoaded && data && data[0].length !== initialTracks.split(',').length) {
+            return;
+          }
+          if (!areInitialTracksLoaded && data && data[0].length === initialTracks.split(',').length) {
+            areInitialTracksLoaded = true;
+            return;
+          }
+          if (areInitialTracksLoaded) {
+            handleButtonState(data[0].toString() !== initialTracks);
+          }
+        } as TracksCallback
+      );
+      JBrowse.subscribe(
+        '/jbrowse/v1/n/globalHighlightChanged',
+        function(data) {
+          const newHighlightSection = `${data[0].ref}:${data[0].start}..${data[0].end}`;
+          handleButtonState(initialHighlight !== newHighlightSection);
+        } as HighlightCallback
+      );
     });
   }
-
-  function updateBehaviors() {
-    const view = jbrowseViewContainer.current;
-    if (view == null) return;
-    if (isLocked) view.behaviorManager.removeAll();
-    else view.behaviorManager.initialize();
+  
+  function onClick() {
+    setKey('jbrowse-iframe-' + Math.floor(Math.random()*1000000));
+    setIsResetButtonDisabled(true);
   }
 
   return (
@@ -77,19 +108,25 @@ export function JbrowseIframe(props: Props) {
           display: 'flex',
           justifyContent: 'flex-end',
           alignItems: 'center',
-          marginBottom: '0.5em'
+          marginBottom: '0.5em',
         }}
       >
-        <label className="jbrowse-scroll-zoom-toggle">
-          <Checkbox
-            style={{ marginRight: '0.25em' }}
-            value={!isLocked}
-            onChange={onCheckboxToggle}
-          />
-          {lockText}
-        </label>
+        <button
+          onClick={onClick}
+          disabled={isResetButtonDisabled}
+        >
+          Reset view
+        </button>
       </div>
-      <iframe onLoad={onLoad} src={jbrowseUrl + "&tracklist=0&nav=1&overview=0&fullviewlink=0&menu=0"} width="100%" height={height} scrolling="no" allowFullScreen={false} />
+      <iframe
+        key={key}
+        onLoad={onLoad}
+        src={jbrowseUrl + "&tracklist=0&nav=1&overview=0&fullviewlink=0&menu=0"}
+        width="100%"
+        height={height}
+        scrolling="no"
+        allowFullScreen={false}
+      />
     </div>
   );
 }
