26761ab691cbbd98d81d71af80a99811ebf7b5d1
Ryan Doherty <tech@conical.org>
2023-03-20 06:37:57 -0400

    Add zipped files reporter form

--- Client/src/util/reporter.js
+++ Client/src/util/reporter.js
@@ -18,6 +18,7 @@ import FastaOrthoSequenceReporterForm from '../components/reporters/FastaOrthoSe
 import TableReporterForm from '../components/reporters/TableReporterForm';
 import TranscriptTableReporterForm from '../components/reporters/TranscriptTableReporterForm';
 import TranscriptAttributesReporterForm from '../components/reporters/TranscriptAttributesReporterForm';
+import ZippedFileReporterForm from '../components/reporters/ZippedFileReporterForm';
 
 let EmptyReporter = props => ( null );
 
@@ -93,6 +94,8 @@ export function selectReporterComponent(reporterName, recordClassFullName) {
       return XmlReporterForm;
     case 'json':
       return JsonReporterForm;
+    case 'zippedFiles':
+      return ZippedFileReporterForm;
     case STANDARD_REPORTER_NAME:
       return WdkServiceJsonReporterForm;
     default:
