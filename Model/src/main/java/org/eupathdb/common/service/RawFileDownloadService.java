d8f5c1dd3f990aa55b5395f6a93230b2aa9da8da
Ryan Doherty <tech@conical.org>
2025-10-02 11:42:40 -0400

    Get matching working and ensure file is downloaded with proper name

--- Model/src/main/java/org/eupathdb/common/service/RawFileDownloadService.java
+++ Model/src/main/java/org/eupathdb/common/service/RawFileDownloadService.java
@@ -3,6 +3,7 @@ package org.eupathdb.common.service;
 import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
@@ -15,19 +16,23 @@ import org.apache.log4j.Logger;
 import org.gusdb.fgputil.Timer;
 import org.gusdb.wdk.service.service.AbstractWdkService;
 
-@Path("/raw-files/{path:[a-zA-Z_0-9\\-\\.]+}")
+@Path("/raw-files/{path:.+}")
 public class RawFileDownloadService extends AbstractWdkService {
 
   private static final Logger LOG = Logger.getLogger(RawFileDownloadService.class);
 
+  private static final Pattern ALLOWED_PATTERN = Pattern.compile("[a-zA-Z_0-9\\-\\./]+");
+
   @GET
   public Response downloadFile(@PathParam("path") String pathSuffix) {
-    if (pathSuffix.contains(".."))
+    if (pathSuffix.contains("..") || !ALLOWED_PATTERN.matcher(pathSuffix).matches()) {
       throw new NotFoundException();
+    }
     String pathPrefix = getWdkModel().getProperties().get("RAWFILESDOWNLOADDIR");
-    java.nio.file.Path path = Paths.get(pathPrefix, pathSuffix);
+    java.nio.file.Path path = Paths.get(pathPrefix, getWdkModel().getProjectId(), pathSuffix);
     LOG.info("User requested download of raw file: " + path);
     if (!Files.isRegularFile(path) || !Files.isReadable(path)) {
+      LOG.warn("Requested file matched regex and seemed value but was not found");
       throw new NotFoundException();
     }
     StreamingOutput streamer = out -> {
@@ -37,7 +42,10 @@ public class RawFileDownloadService extends AbstractWdkService {
         LOG.info("File " + path + ", size " + bytes + " bytes, downloaded in " + t.getElapsedString());
       }
     };
-    return Response.ok(streamer).build();
+    return Response
+        .ok(streamer)
+        .header("Content-Disposition", "attachment; filename=\"" + path.getFileName() + "\"")
+        .build();
   }
 
 }
