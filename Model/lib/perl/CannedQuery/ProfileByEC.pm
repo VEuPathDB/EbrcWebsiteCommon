40f4b8fb06b1c9ef71b8bcc51746be69bb8cf3c3
Steve <43149795+steve-fischer-200@users.noreply.github.com>
2025-10-27 15:14:49 -0400

    webready tables suffix (#242)

--- Model/lib/perl/CannedQuery/ProfileByEC.pm
+++ Model/lib/perl/CannedQuery/ProfileByEC.pm
@@ -15,7 +15,7 @@ sub init {
             select p.source_id, ec.ec_number, p.profile_as_string
             from apidbtuning.profile p,
             (SELECT DISTINCT ta.gene_source_id, ec.ec_number
-            FROM  dots.aaSequenceEnzymeClass asec, sres.enzymeClass ec,webready.TranscriptAttributes ta
+            FROM  dots.aaSequenceEnzymeClass asec, sres.enzymeClass ec,webready.TranscriptAttributes_p ta
             WHERE ta.aa_sequence_id = asec.aa_sequence_id
              AND asec.enzyme_class_id = ec.enzyme_class_id                                                                                                  
              AND ec.ec_number LIKE REPLACE(REPLACE(REPLACE(REPLACE(lower('<<Id>>'),' ',''),'-', '%'),'*','%'),'any','%')                       
