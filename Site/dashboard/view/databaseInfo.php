4b98743e694e977c9d34c119a4b1cee5bbe30688
Mustafa Nural <mvnural@gmail.com>
2026-07-29 14:33:52 -0400

    revamp css theme a little bit.
    Signed-off-by: Mustafa Nural <mvnural@gmail.com>
    (cherry picked from commit 3f1447440a29272ad41d0cc8ac21c756cd16389f)

--- Site/dashboard/view/databaseInfo.php
+++ Site/dashboard/view/databaseInfo.php
@@ -24,7 +24,7 @@ $db_instances_str = $db_instances_attribs['DbInstanceNames'];
 $db_names = explode(',', $db_instances_str);
 
 ?>
-<script type="text/javascript">
+<script>
   $(function () {
     $('#tuningTables').DataTable({
       'stateSave': false,
@@ -51,7 +51,7 @@ if (isset($_GET['refresh']) && $_GET['refresh'] == 1) {
   $success = $database->refresh();
 
   if (!$success)
-    echo "<font color='red'>FAILED TO REFRESH</font>";
+    echo "<span class='fatal'>FAILED TO REFRESH</span>";
 }
 
 $adb = $database->attributes();
@@ -66,63 +66,59 @@ $workflow_status_attrs = $workflow_status->attributes();
 
 <h2><?= $db_display_name ?> Database</h2>
 
-<p>
-  <b>Details</b>:
-<table border="0" cellspacing="3" cellpadding="2" align="">
-  <tr class="secondary3">
-    <th><font size="-2">Property</font></th>
-    <th><font size="-2">Value</font></th>
+<table class="p">
+  <tr>
+    <td><b>Dbname:</b></td>
+    <td class="p"><?= strtolower($adb['db_name']) ?></td>
   </tr>
-  <tr class="rowLight">
-    <td>Db Name</td>
-    <td><?= strtolower($adb['db_name']) ?></td>
+  <tr>
+    <td><b>Aliases:</b></td>
+    <td class="p"><?= implode(", ", $adb_aliases_ar) ?></td>
   </tr>
-  <tr class="rowMedium">
-    <td>Aliases (from LDAP)</td>
-    <td><?= implode(", ", $adb_aliases_ar) ?></td>
+  <tr>
+    <td><b>Hosted on:</b></td>
+    <td class="p"><?= $adb_host === null ? 'unknown' : strtolower($adb_host) ?></td>
   </tr>
-  <tr class="rowLight">
-    <td>Hosted on (from LDAP)</td>
-    <td><?= $adb_host === null ? 'unknown' : strtolower($adb_host) ?></td>
+  <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+  <tr>
+    <td><b>Size on disk:</b></td>
+    <td class="p"><?= $adb['dbf_gb_on_disk'] ?></td>
   </tr>
-  <tr class="rowMedium">
-    <td>Size on disk</td>
-    <td><?= $adb['dbf_gb_on_disk'] ?></td>
+  <tr>
+    <td><b>Version:</b></td>
+    <td class="p"><?= $adb['version'] ?></td>
   </tr>
-  <tr class="rowLight">
-    <td>Version</td>
-    <td><?= $adb['version'] ?></td>
+  <tr>
+    <td><b>Character encoding:</b></td>
+    <td class="p"><?= $adb['character_encoding'] ?></td>
   </tr>
-  <tr class="rowMedium">
-    <td>Character encoding</td>
-    <td><?= $adb['character_encoding'] ?></td>
+  <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+  <tr>
+    <td><b>Client login name:</b></td>
+    <td class="p"><?= strtolower($adb['login']) ?></td>
   </tr>
-  <tr class="rowLight">
-    <td>Client login name</td>
-    <td><?= strtolower($adb['login']) ?></td>
-  </tr>
-  <tr class="rowMedium">
-    <td>Client connecting from</td>
-    <td><?= strtolower($adb['client_host']) ?></td>
+  <tr>
+    <td><b>Client connecting from:</b></td>
+    <td class="p"><?= strtolower($adb['client_host']) ?></td>
   </tr>
 </table>
-</p>
 
 <?php $dblink_map = $adb['DblinkList']; ?>
 <?php if (empty($dblink_map)) { ?>
-<p>
-  <b>Available DBLinks</b>: None
-</p>
+<p><b>Available DBLinks</b>: None</p>
 <?php } else { ?>
-<p>
-  <b>Available DBLinks</b>:
-<table border="0" cellspacing="3" cellpadding="2" align="">
+<p><b>Available DBLinks</b>:</p>
+<div class="table-scroll">
+<table class="dbinfo">
+  <thead>
   <tr class="secondary3">
-    <th align="left"><font size="-2">server</font></th>
-    <th align="left"><font size="-2">type</font></th>
-    <th align="left"><font size="-2">options</font></th>
-    <th align="left"><font size="-2">schemas</font></th>
+    <th>server</th>
+    <th>type</th>
+    <th>options</th>
+    <th>schemas</th>
   </tr>
+  </thead>
+  <tbody>
   <?php
   $row = 0;
   foreach ($dblink_map as $dblink) {
@@ -138,17 +134,17 @@ $workflow_status_attrs = $workflow_status->attributes();
     $row++;
   }
   ?>
+  </tbody>
 </table>
-</p>
+</div>
 <?php } ?>
 
 <hr/>
-<b>Information on this page was last updated</b>: <?= $adb['system_date'] ?><br/>
+<p><b>Information on this page was last updated</b>: <?= $adb['system_date'] ?></p>
 <form method="GET" action="">
   <input name="refresh" type="hidden" value="1">
   <input type="submit" value="update now">
 </form>
-<p>
 
   <?php } ?>
   <!-- End DB sections -->
@@ -164,13 +160,13 @@ $workflow_status_attrs = $workflow_status->attributes();
     <span class='warn'>last_check older than <?= $days_old_warning_threshold ?> days</span>
   </p>
   <div style="display: inline-block; padding-left: 10px;"><!-- constrain jquery datatables -->
-    <table id="tuningTables" class='display' cellspacing="3" cellpadding="2" align="">
+    <table id="tuningTables" class="display">
       <thead>
       <tr class="secondary3">
-        <th align="left"><font size="-2">name</font></th>
-        <th align="left"><font size="-2">last_check</font></th>
-        <th align="left"><font size="-2">status</font></th>
-        <th align="left"><font size="-2">created</font></th>
+        <th>name</th>
+        <th>last_check</th>
+        <th>status</th>
+        <th>created</th>
       </tr>
       </thead>
       <tbody>
@@ -219,12 +215,12 @@ $workflow_status_attrs = $workflow_status->attributes();
 <div class="expandable">
 
   <div style="display: inline-block; padding-left: 10px;"><!-- constrain jquery datatables -->
-    <table id="workflow" class='display' cellspacing="3" cellpadding="2" align="">
+    <table id="workflow" class="display">
       <thead>
       <tr class="secondary3">
-        <th align="left"><font size="-2">step</font></th>
-        <th align="left"><font size="-2">off_line</font></th>
-        <th align="left"><font size="-2">state</font></th>
+        <th>step</th>
+        <th>off_line</th>
+        <th>state</th>
       </tr>
       </thead>
       <tbody>
