06826fd6baef6089e622a906ae74b521bebf1022
Ryan Doherty <tech@conical.org>
2026-08-28 08:50:35 -0400

    Improve log look

--- Model/src/main/java/org/eupathdb/common/service/CyberSourceLogger.java
+++ Model/src/main/java/org/eupathdb/common/service/CyberSourceLogger.java
@@ -8,7 +8,7 @@ public class CyberSourceLogger {
   private static final Logger LOG = Logger.getLogger(CyberSourceLogger.class);
 
   static void logPaymentEvent(String stage, User requestingUser, String referenceNumber, String amount, String currency, String invoiceNumber) {
-    LOG.info(String.join("\t",
+    LOG.info("\t" + String.join("\t",
         stage,
         String.valueOf(requestingUser.getUserId()),
         "guest=" + requestingUser.isGuest(),
